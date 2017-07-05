4cd2c4088f73739bce10168e2cfd99ea7213e822
Jerome Jaglale <jerome_jaglale@sfu.ca>
2017-07-05 14:17:48 -0700

    Update table name in User class

--- app/User.php
+++ app/User.php
@@ -10,6 +10,13 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'user';
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
