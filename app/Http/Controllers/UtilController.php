0ea8d02985c2f6dc28268769fd4d17b9bea1da9f
Jerome Jaglale <jerome_jaglale@sfu.ca>
2017-09-08 16:04:47 -0700

    minor: just for GitHub webhook testing

--- app/Http/Controllers/UtilController.php
+++ app/Http/Controllers/UtilController.php
@@ -58,7 +58,7 @@ class UtilController extends Controller
         $localHash = 'sha1=' . hash_hmac('SHA256', $githubPayload, $localToken);
 
         if (hash_equals($githubHash, $localHash)) {
-            Log::info('-------- Deployment START --------');
+            Log::info('-------- Deployment START -------- ');
 
             $root_path = base_path();
             $process = new Process('cd ' . $root_path . '; ./deploy.sh');
