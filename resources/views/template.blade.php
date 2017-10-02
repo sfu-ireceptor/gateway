39086a6fa3ef3024babed76fda62077700eabed8
Jerome Jaglale <jerome_jaglale@sfu.ca>
2017-10-02 14:05:35 -0700

    minor: HTML fix

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -23,7 +23,7 @@
 
 	<body>
 		<nav class="navbar navbar-default" role="navigation">
-		<div class="container-fluid"
+		<div class="container-fluid">
 		    <!-- Brand and toggle get grouped for better mobile display -->
 			<div class="navbar-header">
 		    	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
