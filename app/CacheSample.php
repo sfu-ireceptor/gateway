ddf0084ffd5b631d006366c443f6aaea3efb1cfa
Jerome Jaglale <jerome_jaglale@sfu.ca>
2017-10-12 14:45:38 -0700

    Add CacheSample class

---
+++ app/CacheSample.php
@@ -0,0 +1,16 @@
+<?php
+namespace App;
+use Jenssegers\Mongodb\Eloquent\Model;
+class CacheSample extends Model
+{
+	protected $connection = 'mongodb';
+    protected $collection = 'samples';
+    protected $guarded = [];
+
+    public static function list($params)
+    {
+    	$l = static::all();
+    	
+        return $l;
+    }
+}
