2159fde4814d6406d2d562b163b8b65e7604abe7
Jerome Jaglale <jerome_jaglale@sfu.ca>
2017-10-17 16:14:54 -0700

    Add "version" to RestService

---
+++ database/migrations/2017_10_17_231007_alter_rest_service_table_add_version.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterRestServiceTableAddVersion extends Migration
+{
+    public function up()
+    {
+        Schema::table('rest_service', function ($table) {
+            $table->integer('version')->default(1);
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('rest_service', function ($table) {
+            $table->dropColumn('version');
+        });
+    }
+}
