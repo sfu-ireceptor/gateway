03ae9973999d4f5aafe5eb8312eb2d1b0c7e0892
Jerome Jaglale <jerome_jaglale@sfu.ca>
2017-10-26 17:07:23 -0700

    Use short names for gateway by default

--- resources/lang/en/sp.php
+++ resources/lang/en/sp.php
@@ -4,9 +4,11 @@ use App\SampleField;
 
 $data = [];
 
-$l = SampleField::all(['ir_id', 'ir_full'])->toArray();
+$l = SampleField::all(['ir_id', 'ir_short'])->toArray();
 foreach ($l as $t) {
-    $data[$t['ir_id']] = $t['ir_full'];
+    $data[$t['ir_id']] = $t['ir_short'];
 }
+
+// var_dump($data);die();
 
 return $data;
