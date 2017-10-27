9d0a30667d829e18355bca52b9702db1413826b0
Brian Corrie <bdcorrie@gmail.com>
2017-10-27 15:09:52 +1300

    Integrated UI layout with new field look up mechanism.

--- resources/views/sample.blade.php
+++ resources/views/sample.blade.php
@@ -3,24 +3,34 @@
 @section('title', 'Search samples')
 
 @section('content')
+
+<?php
+
+use App\RestService;
+
+?>
+
 <div class="container-fluid sample_container">
 	<div class="row">
 		<div class="col-md-12">
 			<ul class="nav nav-tabs nav-justified samples_sequences_nav">
-				<li role="presentation" class="active"><a href="#">Samples</a></li>
-				<li role="presentation"><a href="/sequences">Sequences</a></li>
+				<li role="presentation" class="active"><a href="#"><b>Explore Samples</b></a></li>
+				<li role="presentation"><a href="/sequences"><b> Explore Sequences</b></a></li>
 			</ul>
 		</div>
 	</div>
 
 	<div class="row">
 		<div class="col-md-2">
+		<div class="data_container_box">	
 			{{ Form::open(array('url' => 'samples', 'role' => 'form', 'method' => 'get', 'class' => 'sample_search')) }}
 				<input type="hidden" name="project_id_list" />
-
+				<p>
+					{{ Form::submit('Update Filters', array('class' => 'btn btn-primary search_samples')) }}
+				</p>
 			    <div class="form-group">
-					{{ Form::label('subject_id', __('sp.subject_id')) }}
-					{{ Form::text('subject_id', '', array('class' => 'form-control')) }}
+					{{ Form::label('subject_code', __('sp.subject_id')) }}
+					{{ Form::text('subject_code', '', array('class' => 'form-control')) }}
 				</div>
 
 				<div class="form-group">
@@ -46,28 +56,28 @@
 				</div>
 
 			    <div class="form-group">
-					{{ Form::label('sample_id',  __('sp.sample_id')) }}
-					{{ Form::text('sample_id', '', array('class' => 'form-control')) }}
+					{{ Form::label('sample_name', __('sp.sample_id')) }}
+					{{ Form::text('sample_name', '', array('class' => 'form-control')) }}
 				</div>
 
 				<div class="form-group">
-					{{ Form::label('cell_subset',  __('sp.cell_subset')) }}
+					{{ Form::label('ireceptor_cell_subset_name', __('sp.cell_subset')) }}
 					@foreach ($cell_type_list as $id => $name)
 					<div class="checkbox">
 						<label>
-						{{ Form::checkbox('cell_subset[]', $id) }}
+						{{ Form::checkbox('ireceptor_cell_subset_name[]', $id) }}
 						{{ $name }}
 						</label>
 					</div>
 					@endforeach
 				</div>
 
- 			    <div class="form-group">
-					{{ Form::label('tissue',  __('sp.tissue')) }}
+			    <div class="form-group">
+					{{ Form::label('sample_source_name', __('sp.tissue')) }}
 					@foreach ($sample_source_list as $id => $name)
 					<div class="checkbox">
 						<label>
-						{{ Form::checkbox('tissue[]', $id) }}
+						{{ Form::checkbox('sample_source_name[]', $id) }}
 						{{ $name }}
 						</label>
 					</div>
@@ -75,7 +85,7 @@
 				</div>
 
 				 <div class="form-group">
-					{{ Form::label('dna_type',  __('sp.library_source')) }}
+					{{ Form::label('dna_type', __('sp.library_source')) }}
 					@foreach ($dna_type_list as $id => $name)
 					<div class="checkbox">
 						<label>
@@ -86,16 +96,54 @@
 					@endforeach
 				</div>
 
-
 				<div id="results"></div>
-				{{ Form::submit('Search samples', array('class' => 'btn btn-primary search_samples')) }}
 			{{ Form::close() }}				
+		</div>
 		</div>
 
 		<div class="col-md-10">
-			
-			<!-- statistics box -->
-			<div class="samples_stats">
+			<div class="data_container_box">
+				<b>Active filters:</b>
+				<?php /*
+				@foreach ($filters as $filter)
+				<span class="data_text_box">
+					{{$filter}}
+				</span>
+				@endforeach
+				*/ ?>
+			</div>
+			<div class="data_container_box">
+				<b>Query breadth</b>:
+				<?php /*
+				<span class="data_text_box">
+					{{$totalRepositories}} remote repositories
+				</span>
+				<span class="data_text_box">
+					{{$totalLabs}} research labs
+				</span>
+				<span class="data_text_box">
+					{{$totalStudies}} studies
+				</span>
+				<span class="data_text_box">
+					{{$totalSamples}} samples
+				</span>
+				<span class="data_text_box">
+					{{number_format($totalSequences)}} sequences
+				</span>
+				*/ ?>
+			</div>
+			<div class="data_container_box">
+				<b>Query result:</b>
+				<?php /*
+				<span class="data_text_box">
+					{{$nFilteredSamples}} samples
+				</span>
+				<span class="data_text_box">
+					{{number_format($nFilteredSequences)}} sequences.
+				</span>
+				*/ ?>
+			</div>
+			<div class="data_container_box">
 				<div id="sample_charts" class="charts">
 					<div class="row">
 						<div class="col-md-2 chart" id="sample_chart1"></div>
@@ -108,22 +156,30 @@
 				</div>
 			</div>
 
+			<div class="data_container_box">
 			@if (! empty($sample_list))
 			{{ Form::open(array('url' => 'sequences', 'role' => 'form', 'method' => 'get')) }}
 
+				<?php
+					$max_count = 10;
+				?>
 
-				{{ Form::submit('Browse sequences', array('class' => 'btn btn-primary browse-seq-data-button', 'disabled' => 'disabled')) }}
+				<?php /* if ($nFilteredSamples > $max_count): ?>
+					{{ Form::submit('View all samples ('.(string)($nFilteredSamples-$max_count).' more)', array('class' => 'btn btn-primary show-more-samples-button', 'disabled' => 'disabled')) }}
+				<?php endif	*/ ?>
+
+				{{ Form::submit('Browse sequences from selected samples', array('class' => 'btn btn-primary browse-seq-data-button', 'disabled' => 'disabled')) }}
 
 				<table class="table table-striped sample_list">
 					<thead>
 						<tr>
-							<th></th>
+							<th>{{ Form::checkbox('select_all') }}</th>
 							<th>Repository</th>
 							<th>{{ __('sp.lab_name') }}</th>
 							<th>{{ __('sp.study_title') }}</th>
-							<th>{{ __('sp.sample_id') }}</th>
-							<th>Sequences</th>
 							<th>{{ __('sp.subject_id') }}</th>
+							<th>Sequences</th>
+							<th>{{ __('sp.sample_id') }}</th>
 							<th>{{ __('sp.tissue') }}</th>
 							<th>{{ __('sp.cell_subset') }}</th>
 							<th>{{ __('sp.cell_phenotype') }}</th>
@@ -131,84 +187,133 @@
 						</tr>
 					</thead>
 					<tbody>
+						<?php
+							$count = 0;
+						?>
 						@foreach ($sample_list as $sample)
 						<tr>
-							<td>{{ Form::checkbox('ir_project_sample_id_list_' . $sample->rest_service_id . '[]', $sample->ir_project_sample_id) }}</td>
-							<td class="text-nowrap">{{ $sample->rest_service_name }}</td>
+							<td>{{ Form::checkbox('project_sample_id_list_' . $sample->rest_service_id . '[]', $sample->ir_project_sample_id) }}</td>
 							<td class="text-nowrap">
-								@isset($sample->lab_name)
+								<span title="{{ $sample->rest_service_name }}">
+									{{ str_limit($sample->rest_service_name, $limit = 16, $end = '...') }}
+								</span>
+							</td>
+							<td class="text-nowrap">
 									<span title="{{ $sample->lab_name }}">
 									{{ str_limit($sample->lab_name, $limit = 20, $end = '...') }}
 									</span>
-								@endisset
 							</td>
 							<td>
-								@if(isset($sample->sra_accession))
+								<?php if (isset($sample->sra_accession)): ?>
 									<a href="https://trace.ncbi.nlm.nih.gov/Traces/sra/?study={{ $sample->sra_accession }}" title="{{ $sample->study_title }}">
 										{{ str_limit($sample->study_title, $limit = 20, $end = '...') }}
 									</span>
-								@elseif(isset($sample->study_title))
+								<?php elseif (isset($sample->study_title)): ?>
 									<span title="{{ $sample->study_title }}">
 									{{ str_limit($sample->study_title, $limit = 20, $end = '...') }}
 									</span>							
-								@endif
+								<?php endif ?>
 							</td>
 							<td>
-								@isset($sample->sample_id)
-									{{ $sample->sample_id }}
+								@isset($sample->subject_id)
+									<span title="{{ $sample->subject_id }}">
+									{{ str_limit($sample->subject_id, $limit = 12, $end = '...') }}
+									</span>
 								@endisset
-							</td>
+							</td>							
+						
 							<td>
 								@isset($sample->ir_sequence_count)
 									@if ($sample->ir_sequence_count > 0)
-									<a href="sequences?ir_project_sample_id_list_{{ $sample->rest_service_id }}[]={{ $sample->ir_project_sample_id }}">
-										<span class="label label-primary">{{number_format($sample->ir_sequence_count, 0 ,'.' ,',') }}</span>
-									</a>
+										<a href="sequences?project_sample_id_list_{{ $sample->rest_service_id }}[]={{ $sample->ir_project_sample_id }}">
+											<span class="label label-primary">{{number_format($sample->ir_sequence_count, 0 ,'.' ,',') }}</span>
+										</a>
 									@endif
 								@endisset
 							</td>
 							<td>
-								@isset($sample->subject_id)
-									{{ $sample->subject_id }}
+								@isset($sample->sample_id)
+									<span title="{{ $sample->sample_id }}">
+									{{ str_limit($sample->sample_id, $limit = 12, $end = '...') }}
+									</span>
 								@endisset
-							</td>
+							</td>	
 							<td>
 								@isset($sample->tissue)
-									{{ $sample->tissue }}
+									<span title="{{ $sample->tissue }}">
+									{{ str_limit($sample->tissue, $limit = 12, $end = '...') }}
+									</span>
 								@endisset
 							</td>
 							<td>
 								@isset($sample->cell_subset)
-									{{ $sample->cell_subset }}
+									<span title="{{ $sample->cell_subset }}">
+									{{ str_limit($sample->cell_subset, $limit = 12, $end = '...') }}
+									</span>
 								@endisset
-							</td>
 							<td>
 								@isset($sample->cell_phenotype)
-									{{ $sample->cell_phenotype }}
+									<span title="{{ $sample->cell_phenotype }}">
+									{{ str_limit($sample->cell_phenotype, $limit = 12, $end = '...') }}
+									</span>
 								@endisset
 							</td>
 							<td>
 								@isset($sample->library_source)
-									{{ $sample->library_source }}
+									<span title="{{ $sample->library_source }}">
+									{{ str_limit($sample->library_source, $limit = 12, $end = '...') }}
+									</span>
 								@endisset
 							</td>
 						</tr>
+						<?php /*
+							$count = $count + 1;
+							if ($count >= $max_count) break;
+						*/ ?>
 						@endforeach
 					</tbody>
 				</table>
 
 				<input type="hidden" name="project_id_list" />
 				
-				{{ Form::submit('Browse sequences', array('class' => 'btn btn-primary browse-seq-data-button', 'disabled' => 'disabled')) }}
+				<?php /*if ($nFilteredSamples > $max_count): */?>
+					<?php /*
+					{{ Form::submit('View all samples ('.(string)($nFilteredSamples-$max_count).' more)', array('class' => 'btn btn-primary show-more-samples-button', 'disabled' => 'disabled')) }}
+					*/ ?>
+				<?php /* endif	*/ ?>
+				{{ Form::submit('Browse sequences from selected samples', array('class' => 'btn btn-primary browse-seq-data-button', 'disabled' => 'disabled')) }}
 
 			{{ Form::close() }}
 			@endif
+			</div>
 
 		</div>
 	</div>
 </div>
 
 <script>
-var data = {!! $sample_list_json !!};
+/*
+// v2 names
+var graphFields = [
+        "study_type", "organism", "disease_state_sample",
+        "tissue", "cell_subset", "library_source"
+    ];
+// v1 names
+var graphFields = [
+        "project_type", "subject_species", "disease_state_name",
+        "sample_source_name", "ireceptor_cell_subset_name", "dna_type"
+    ];
+*/
+var graphFields = [
+        "project_type", "subject_species", "disease_state_name",
+        "sample_source_name", "ireceptor_cell_subset_name", "dna_type"
+    ];
+var graphNames = [
+        "Study Type", "Organism", "Sample Disease State",
+        "Sample Type", "Cell Subset", "Target Substrate"
+    ];
+var graphDIV = "sample_chart";
+var graphInternalLabels = true;
+var graphData = {!! $sample_list_json !!};
 </script>
 @stop
