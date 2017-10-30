91f6bde1c0829e5fde4efd451f7336c01d5f9ea9
Jerome Jaglale <jerome_jaglale@sfu.ca>
2017-10-30 11:41:21 -0700

    Add MongoDB node to RestService seeder

--- database/seeds/RestServiceSeeder.php
+++ database/seeds/RestServiceSeeder.php
@@ -13,17 +13,26 @@ class RestServiceSeeder extends Seeder
               'name' => 'iReceptor Public Archive',
               'username' => '',
               'password' => '',
+              'version' => 2,
             ],
             [
               'url' => 'https://vdj-dev.tacc.utexas.edu/ireceptor/',
               'name' => 'VDJServer',
               'username' => '',
               'password' => '',
+              'version' => 2,
             ],
-        ];
+            [
+              'url' => 'https://206.12.99.171/',
+              'name' => 'iReceptor Public Archive 2 (MongoDB)',
+              'username' => '',
+              'password' => '',
+              'version' => 2,
+            ],
+            ];
 
         foreach ($l as $t) {
-          RestService::firstOrCreate(['url' => $t['url']], ['name' => $t['name']]);
+          RestService::firstOrCreate(['url' => $t['url']], $t);
         }
     }
 }
