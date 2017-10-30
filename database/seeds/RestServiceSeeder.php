b94ff23c8fecf7f12b12d69ad53872897ef8430a
Jerome Jaglale <jerome_jaglale@sfu.ca>
2017-10-30 11:44:13 -0700

    minor: formatting

--- database/seeds/RestServiceSeeder.php
+++ database/seeds/RestServiceSeeder.php
@@ -1,38 +1,38 @@
 <?php
 
-use App\RestService;
 use Illuminate\Database\Seeder;
+use App\RestService;
 
 class RestServiceSeeder extends Seeder
 {
-    public function run()
-    {
-        $l = [
-            [
-              'url' => 'https://ipa.ireceptor.org/',
-              'name' => 'iReceptor Public Archive',
-              'username' => '',
-              'password' => '',
-              'version' => 2,
-            ],
-            [
-              'url' => 'https://vdj-dev.tacc.utexas.edu/ireceptor/',
-              'name' => 'VDJServer',
-              'username' => '',
-              'password' => '',
-              'version' => 2,
-            ],
-            [
-              'url' => 'https://206.12.99.171/',
-              'name' => 'iReceptor Public Archive 2 (MongoDB)',
-              'username' => '',
-              'password' => '',
-              'version' => 2,
-            ],
-            ];
+public function run()
+{
+$l = [
+[
+'url' => 'https://ipa.ireceptor.org/',
+'name' => 'iReceptor Public Archive',
+'username' => '',
+'password' => '',
+'version' => 2,
+],
+[
+'url' => 'https://vdj-dev.tacc.utexas.edu/ireceptor/',
+'name' => 'VDJServer',
+'username' => '',
+'password' => '',
+'version' => 2,
+],
+[
+'url' => 'https://206.12.99.171/',
+'name' => 'iReceptor Public Archive 2 (MongoDB)',
+'username' => '',
+'password' => '',
+'version' => 2,
+],
+];
 
-        foreach ($l as $t) {
-            RestService::firstOrCreate(['url' => $t['url']], $t);
-        }
-    }
+foreach ($l as $t) {
+RestService::firstOrCreate(['url' => $t['url']], $t);
+}
+}
 }
