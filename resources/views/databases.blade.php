afa6aba0a8bf63ce9cbd6d1a706cfd0e5612d632
Jerome Jaglale <jerome_jaglale@sfu.ca>
2017-10-31 15:24:13 -0700

    Add useful links to admin databases page

--- resources/views/databases.blade.php
+++ resources/views/databases.blade.php
@@ -14,12 +14,14 @@
 	@endif
 
 	<div class="row">
-		<div class="col-md-4">
+		<div class="col-md-12">
 
 			<table class="table table-bordered table-striped rs_list">
 				<thead>
 					<tr>
 						<th>Enabled</th>
+						<th>Name</th>
+						<th>URL</th>
 						<th></th>
 					</tr>
 				</thead>
@@ -27,7 +29,10 @@
 					@foreach ($rs_list as $rs)
 						<tr>
 							<td>{{ Form::checkbox('rs_enabled', $rs->id, $rs->enabled) }}</td>					
-							<td>{{ $rs->name }}</td>					
+							<td>{{ $rs->name }}</td>	
+							<td><a href="{{ $rs->url }}">{{ $rs->url }}</a></td>					
+							<td><a href="{{ $rs->url }}v2/samples?username={{ Auth::user()->username }}">/v2/samples</a></td>
+							<td><a href="{{ $rs->url }}v2/sequences_summary?username={{ Auth::user()->username }}">v2/sequences_summary</a></td>			
 						</tr>
 					@endforeach
 				</tbody>
