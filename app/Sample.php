815c86af49f75e69b9ac9d593bec0ace11a1292d
Jerome Jaglale <jerome_jaglale@sfu.ca>
2017-11-06 13:09:41 -0800

    Update gateway metadata field for "Target Substrate"

--- app/Sample.php
+++ app/Sample.php
@@ -34,7 +34,7 @@ class Sample extends Model
         $t = [];
 
         // distinct values for simple sample fields
-        $fields = ['study_group_description', 'library_source', 'ethnicity', 'tissue', 'sex', 'cell_subset'];
+        $fields = ['study_group_description', 'template_class', 'ethnicity', 'tissue', 'sex', 'cell_subset'];
         foreach ($fields as $field) {
             $t[$field] = self::distinctValues($field);
         }
