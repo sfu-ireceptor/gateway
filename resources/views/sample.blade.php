669a4b4023ace35779070d198ca28c1b794f5e7c
Brian Corrie <bdcorrie@gmail.com>
2017-11-09 09:58:42 +1300

    Created a third filter pull down for study. Added a couple of filters and display columns for testing... Can remove display columns later if desired...

--- resources/views/sample.blade.php
+++ resources/views/sample.blade.php
@@ -19,26 +19,12 @@
 						<div class="panel-heading" role="tab" id="headingOne">
 							<h4 class="panel-title">
 								<a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-									Filter by subject
+									Filter by study
 								</a>
 							</h4>
 						</div>
 						<div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
 							<div class="panel-body">
-							    <div class="form-group">
-									{{ Form::label('subject_id', __('short.subject_id')) }}
-									{{ Form::text('subject_id', '', array('class' => 'form-control')) }}
-								</div>
-
-								<div class="form-group">
-									{{ Form::label('sex', __('short.sex')) }}
-									{{ Form::select('sex', $subject_gender_list, '', array('class' => 'form-control')) }}
-								</div>
-
-							    <div class="form-group">
-									{{ Form::label('ethnicity', __('short.ethnicity')) }}
-									{{ Form::select('ethnicity', $subject_ethnicity_list, '', array('class' => 'form-control')) }}
-								</div>
 
 							    <div class="form-group">
 									{{ Form::label('study_id', __('short.study_id')) }}
@@ -56,8 +42,41 @@
 								</div>
 
 							    <div class="form-group">
+									{{ Form::label('study_group_description', __('short.study_group_description')) }}
+									{{ Form::text('study_group_description', '', array('class' => 'form-control')) }}
+								</div>
+
+							    <div class="form-group">
 									{{ Form::label('lab_name', __('short.lab_name')) }}
 									{{ Form::text('lab_name', '', array('class' => 'form-control')) }}
+								</div>
+
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="headingTwo">
+							<h4 class="panel-title">
+								<a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+									Filter by subject
+								</a>
+							</h4>
+						</div>
+						<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+							<div class="panel-body">
+							    <div class="form-group">
+									{{ Form::label('subject_id', __('short.subject_id')) }}
+									{{ Form::text('subject_id', '', array('class' => 'form-control')) }}
+								</div>
+
+								<div class="form-group">
+									{{ Form::label('sex', __('short.sex')) }}
+									{{ Form::select('sex', $subject_gender_list, '', array('class' => 'form-control')) }}
+								</div>
+
+							    <div class="form-group">
+									{{ Form::label('ethnicity', __('short.ethnicity')) }}
+									{{ Form::select('ethnicity', $subject_ethnicity_list, '', array('class' => 'form-control')) }}
 								</div>
 
 							    <div class="form-group">
@@ -80,14 +99,14 @@
 						</div>
 					</div>
 					<div class="panel panel-default">
-						<div class="panel-heading" role="tab" id="headingTwo">
+						<div class="panel-heading" role="tab" id="headingThree">
 							<h4 class="panel-title">
-								<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+								<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
 									Filter by sample
 								</a>
 							</h4>
 						</div>
-						<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+						<div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
 							<div class="panel-body">
 								<div class="form-group">
 									{{ Form::label('sample_id', __('short.sample_id')) }}
@@ -210,6 +229,8 @@
 							<th>Repository</th>
 							<th>@lang('short.lab_name')</th>
 							<th>@lang('short.study_title')</th>
+							<th>@lang('short.study_id')</th>
+							<th>@lang('short.study_group_description')</th>
 							<th>@lang('short.subject_id')</th>
 							<th>Sequences</th>
 							<th>@lang('short.sample_id')</th>
@@ -249,6 +270,20 @@
 									</span>							
 								@endif
 							</td>
+							<td>
+								@isset($sample->study_id)
+									<span title="{{ $sample->study_id }}">
+									{{ str_limit($sample->study_id, $limit = 15, $end = '‥') }}
+									</span>
+								@endisset
+							</td>		
+							<td>
+								@isset($sample->study_group_description)
+									<span title="{{ $sample->study_group_description }}">
+									{{ str_limit($sample->study_group_description, $limit = 15, $end = '‥') }}
+									</span>
+								@endisset
+							</td>	
 							<td>
 								@isset($sample->subject_id)
 									<span title="{{ $sample->subject_id }}">
