79051e1801c30066aa1522f24e410eeacbb79241
Jerome Jaglale <jerome_jaglale@sfu.ca>
2017-11-08 11:53:05 -0800

    minor

--- app/RestService.php
+++ app/RestService.php
@@ -169,13 +169,10 @@ class RestService extends Model
             }
 
             $data['items'] = array_merge($obj->items, $data['items']);
-
-            // convert any v1 fields to v2
-            $data['items'] = FieldName::convertObjectList($data['items'], 'ir_v1', 'ir_v2');
-
             $data['summary'] = array_merge($obj->summary, $data['summary']);
 
             // convert any v1 fields to v2
+            $data['items'] = FieldName::convertObjectList($data['items'], 'ir_v1', 'ir_v2');
             $data['summary'] = FieldName::convertObjectList($data['summary'], 'ir_v1', 'ir_v2');
 
             $rs_data = [];
