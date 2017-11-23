c836e211495daba5d4b95130d1d620067a7d8345
Brian Corrie <bdcorrie@gmail.com>
2017-11-23 16:06:42 +1300

    Added about, logos to the login page.

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -7,8 +7,11 @@
 
 	<div class="row">
 		<div class="col-md-3">
+			<img src="/images/logos/ireceptor_logo.png" height=64>
+			<span class="login-logo-text">iReceptor</span>
+
 		</div>
-		<div class="col-md-8">
+		<div class="col-md-9">
 			<div class="intro">
 				<p>A gateway to curated, distributed databases containing "Next Generation" sequence data from immune responses available for exploration, analysis, and download.</p> 
 			</div>
@@ -93,9 +96,70 @@
 		</div>
 
 	</div>
+	<div class="data_container_box">
+	</div>
 
+	<div class="row">
+		<div class="col-md-9">
+			<h3>
+			About iReceptor
+			</h3>
+			<p>
+			iReceptor is a distributed data management system and scientific gateway for mining “Next Generation”sequence data from immune responses. The goal of the project is to: improve the design of vaccines, therapeutic antibodies and cancer immunotherapies by integrating Canadian and international data repositories of antibody and T-cell receptor gene sequences.
+			</p>
+			<p>
+			iReceptor provides a technology platform that will lower the barrier to immune genetics researchers who need to federate large, distributed, immune genetics data repositories in order to answer complex questions about the immune response. The focus of the iReceptor project is to leverage existing capabilities and technologies to build a new scientific platform for the immune genetics research community.
+			</p>
+		</div>
+		<div class="col-md-3">
+			<h3>
+			Contact/Resources
+			</h3>
+			<p>
+				To ask question or get involved, email: support@ireceptor.org
+			</p>
+			<p>
+				To learn more about iReceptor visit the <a href="http://www.ireceptor.org" target="_blank">iReceptor website</a>.
+			</p>
+			<p>
+				To learn more about the Adaptive Immune Response Repertoire (AIRR) Community, visit the
+				<a href="http://www.airr-community.org" target="_blank">AIRR website</a>
+			</p>
+		</div>
+	</div>
+	<div class="data_container_box">
+	</div>
 
-</div>
+	<div class="row">
+		<div class="col-md-4">
+			<h4>Funded by</h4>
+			<a href="http://www.innovation.ca" class="cfi">
+				<img src="/images/logos/cfi.png"><br />
+			</a>
+			<a href="http://www2.gov.bc.ca/gov/content/governments/about-the-bc-government/technology-innovation/bckdf" class="bckdf">
+				<img src="/images/logos/bckdf.png"><br />
+			</a>
+			<a href="http://www.canarie.ca" class="canarie">
+				<img src="/images/logos/canarie.png">
+			</a>
+		</div>
+		<div class="col-md-4">
+			<h4>Developed and Run by</h4>
+			<a href="http://www.irmacs.sfu.ca/" class="irmacs">
+				<img src="/images/logos/irmacs.png">
+			</a>
+		</div>
+		<div class="col-md-4">
+			<h4 class="powered">Powered by</h4>
+			<a href="https://www.computecanada.ca/" class="compute_canada">
+				<img src="/images/logos/compute_canada.png">
+			</a>
+			<a href="http://agaveapi.co/" class="agave">
+				<img src="/images/logos/agave.png">
+			</a>
+		</div>
+
+	</div>
 </div>
 
 <script>
