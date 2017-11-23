abd4a3cc4de43b392a8d66b7a16fb82da6c17954
Brian Corrie <bdcorrie@gmail.com>
2017-11-24 11:26:11 +1300

    Added sample_id and subject_id to the quick search page (replacing organism and template_class).

--- resources/views/sequenceQuickSearch.blade.php
+++ resources/views/sequenceQuickSearch.blade.php
@@ -192,19 +192,19 @@
 <script>
 	var graphFields = [
 	        "@lang('v2.study_description')", 
-	        "@lang('v2.organism')",
+	        "@lang('v2.ir_subject_id')",
+	        "@lang('v2.ir_sample_id')",
 	        "@lang('v2.disease_state_sample')", 
 	        "@lang('v2.tissue')",
-	        "@lang('v2.cell_subset')", 
-	        "@lang('v2.template_class')"
+	        "@lang('v2.cell_subset')"
 	    ];
 	var graphNames = [
 	        "@lang('short.study_description')",
-	        "@lang('short.organism')", 
+	        "@lang('short.subject_id')", 
+	        "@lang('short.sample_id')",
 	        "@lang('short.disease_state_sample')",
 	        "@lang('short.tissue')", 
-	        "@lang('short.cell_subset')", 
-	        "@lang('short.template_class')"
+	        "@lang('short.cell_subset')"
 	    ];
 var graphDIV = "sequence_chart";
 var graphInternalLabels = true;
