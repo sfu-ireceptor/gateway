9dd236294f2f723e91b45a8b7796a67fee197d69
Jerome Jaglale <jerome_jaglale@sfu.ca>
2017-11-29 14:09:52 -0800

    Add loading message to samples and quick search pages

--- resources/views/sample.blade.php
+++ resources/views/sample.blade.php
@@ -448,6 +448,17 @@
 	</div>
 </div>
 
+<div class="loading_message">
+	<h2>Loading...</h2>
+	<p>This could take a few seconds to few minutes.</p>
+	<p class="text-right cancel_container">
+		<a href="#" class="btn btn-warning cancel">
+			<span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span>
+			<span class="text loading">Cancel</span>
+		</a>
+	</p>
+</div>
+
 <script>
 	var graphFields = [
 	        "@lang('v2.study_description')", 
