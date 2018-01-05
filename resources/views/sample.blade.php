6af304370aa510f06a03d67facf49960f527e27f
Jerome Jaglale <jerome_jaglale@sfu.ca>
2018-01-05 14:56:42 -0800

    Fix typo

--- resources/views/sample.blade.php
+++ resources/views/sample.blade.php
@@ -450,7 +450,7 @@
 
 <div class="loading_message">
 	<h2>Loading...</h2>
-	<p>This could take a few seconds to few minutes.</p>
+	<p>This could take a few seconds to a few minutes.</p>
 	<p class="text-right cancel_container">
 		<a href="#" class="btn btn-warning cancel">
 			<span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span>
