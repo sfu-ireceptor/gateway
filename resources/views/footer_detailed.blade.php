269f7fe0a32943c0394931f038874ddbadb7c34e
Jerome Jaglale <jerome_jaglale@sfu.ca>
2018-02-05 12:37:39 -0800

    Add re-styled footer to home page

---
+++ resources/views/footer_detailed.blade.php
@@ -0,0 +1,57 @@
+<div class="row">
+	<div class="col-md-6">
+		<div class="login_about_box">
+			<h3>About iReceptor</h3>
+			<p>
+				iReceptor is a distributed data management system and scientific gateway for mining “Next Generation”sequence data from immune responses. The goal of the project is to: improve the design of vaccines, therapeutic antibodies and cancer immunotherapies by integrating Canadian and international data repositories of antibody and T-cell receptor gene sequences.
+			</p>
+			<p>
+				iReceptor provides a technology platform that will lower the barrier to immune genetics researchers who need to federate large, distributed, immune genetics data repositories in order to answer complex questions about the immune response. The focus of the iReceptor project is to leverage existing capabilities and technologies to build a new scientific platform for the immune genetics research community.
+			</p>
+			<p>
+				For more information, see the <a href="http://www.ireceptor.org" target="_blank">iReceptor public website</a>.
+			</p>
+		</div>
+	</div>
+	<div class="col-md-2"></div>
+	<div class="col-md-3">
+	<div class="login_about_box">
+		<h3>Contact</h3>
+		<p>
+			To ask questions or get involved, email:<br>
+			<a href="mailto:support@ireceptor.org">support@ireceptor.org</a>
+		</p>
+
+	</div>
+	</div>
+</div>
+
+<div class="row credits">
+	<div class="col-md-4">
+		<h4>Funded by</h4>
+		<a href="http://www.innovation.ca" class="cfi">
+			<img src="/images/logos/cfi.png"><br />
+		</a>
+		<a href="http://www2.gov.bc.ca/gov/content/governments/about-the-bc-government/technology-innovation/bckdf" class="bckdf">
+			<img src="/images/logos/bckdf.png"><br />
+		</a>
+		<a href="http://www.canarie.ca" class="canarie">
+			<img src="/images/logos/canarie.png">
+		</a>
+	</div>
+	<div class="col-md-4">
+		<h4>Developed and Run by</h4>
+		<a href="http://www.irmacs.sfu.ca/" class="irmacs">
+			<img src="/images/logos/irmacs.png">
+		</a>
+	</div>
+	<div class="col-md-4">
+		<h4 class="powered">Powered by</h4>
+		<a href="https://www.computecanada.ca/" class="compute_canada">
+			<img src="/images/logos/compute_canada.png">
+		</a>
+		<a href="http://agaveapi.co/" class="agave">
+			<img src="/images/logos/agave.png">
+		</a>
+	</div>
+</div>
