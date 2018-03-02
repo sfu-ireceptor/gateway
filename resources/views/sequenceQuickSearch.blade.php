437ced45954ff3e8a1769b64fe5bd090b863fb01
Jerome Jaglale <jerome_jaglale@sfu.ca>
2018-03-02 13:09:50 -0800

    Add help text for junction aa field on sequence quick search page

--- resources/views/sequenceQuickSearch.blade.php
+++ resources/views/sequenceQuickSearch.blade.php
@@ -25,7 +25,7 @@
 						<div class="panel-body">
 							<div class="form-group">
 								{{ Form::label('junction_aa', $filters_list_all['junction_aa']) }}
-								{{ Form::text('junction_aa', '', array('class' => 'form-control', 'minlength' => '4')) }}
+								{{ Form::text('junction_aa', '', array('class' => 'form-control', 'minlength' => '4', 'data-toggle' => 'tooltip', 'title' => 'Substring search (matches entire substring provided, minimum of 4 AA required). Will take a long time if millions of sequences are found.', 'data-placement' => 'bottom')) }}
 							</div>
 						</div>
 					</div>
