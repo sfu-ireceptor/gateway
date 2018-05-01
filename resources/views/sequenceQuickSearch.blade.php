a08a663982f0ed2f9991fc3e3a28bbe126aac172
Jerome Jaglale <jerome_jaglale@sfu.ca>
2018-05-01 13:47:58 -0700

    Move "loading" message div to separate view

--- resources/views/sequenceQuickSearch.blade.php
+++ resources/views/sequenceQuickSearch.blade.php
@@ -245,16 +245,7 @@
 	</div>
 </div>
 
-<div class="loading_message">
-	<h2>Loading...</h2>
-	<p>This could take a few seconds to a few minutes.</p>
-	<p class="text-right cancel_container">
-		<a href="#" class="btn btn-warning cancel">
-			<span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span>
-			<span class="text loading">Cancel</span>
-		</a>
-	</p>
-</div>
+@include('loadingMessage')
 
 <script>
 	var graphFields = [
