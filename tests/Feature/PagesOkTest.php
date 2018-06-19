22b5b9818f79780d084b591f2a834a6b88844c74
Jerome Jaglale <jerome_jaglale@sfu.ca>
2018-06-19 13:02:16 -0700

    Add other page tests

--- tests/Feature/PagesOkTest.php
+++ tests/Feature/PagesOkTest.php
@@ -6,12 +6,34 @@ use Tests\TestCase;
 
 class PagesOkTest extends TestCase
 {
+    /*
+    |--------------------------------------------------------------------------
+    | Public
+    |--------------------------------------------------------------------------
+    */
     /** @test */
     public function login()
     {
         $this->get('/login')->assertOk();
     }
 
+    /** @test */
+    public function forgot_password()
+    {
+        $this->get('/user/forgot-password')->assertOk();
+    }
+
+    /** @test */
+    public function about()
+    {
+        $this->get('/about')->assertOk();
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Require authentication
+    |--------------------------------------------------------------------------
+    */
     /** @test */
     public function home()
     {
@@ -31,5 +53,102 @@ class PagesOkTest extends TestCase
     {
         $u = factory(\App\User::class)->make();
         $this->actingAs($u)->followingRedirects()->get('/sequences')->assertOk();
+    }
+
+    /** @test */
+    public function sequences_quick_search()
+    {
+        $u = factory(\App\User::class)->make();
+        $this->actingAs($u)->get('/sequences-quick-search')->assertOk();
+    }
+
+    /** @test */
+    public function bookmarks()
+    {
+        $u = factory(\App\User::class)->make();
+        $this->actingAs($u)->get('/bookmarks')->assertOk();
+    }
+
+        /** @test */
+    public function systems()
+    {
+        $u = factory(\App\User::class)->make();
+        $this->actingAs($u)->get('/systems')->assertOk();
+    }
+
+    /** @test */
+    public function jobs()
+    {
+        $u = factory(\App\User::class)->make();
+        $this->actingAs($u)->get('/jobs')->assertOk();
+    }
+
+    /** @test */
+    // public function account()
+    // {
+    //     $u = factory(\App\User::class)->make();
+    //     $this->actingAs($u)->get('/user/account')->assertOk();
+    // }
+
+    /** @test */
+    // public function change_personal_info()
+    // {
+    //     $u = factory(\App\User::class)->make();
+    //     $this->actingAs($u)->get('/user/change-personal-info')->assertOk();
+    // }
+
+    /** @test */
+    public function change_password()
+    {
+        $u = factory(\App\User::class)->make();
+        $this->actingAs($u)->get('/user/change-password')->assertOk();
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Admin
+    |--------------------------------------------------------------------------
+    */
+
+    /** @test */
+    public function databases()
+    {
+        $u = factory(\App\User::class)->make();
+        $this->actingAs($u)->get('/admin/databases')->assertOk();
+    }
+
+    /** @test */
+    public function news()
+    {
+        $u = factory(\App\User::class)->make();
+        $this->actingAs($u)->get('/admin/news')->assertOk();
+    }
+
+    /** @test */
+    public function queues()
+    {
+        $u = factory(\App\User::class)->make();
+        $this->actingAs($u)->get('/admin/queues')->assertOk();
+    }
+
+    /** @test */
+    // public function users()
+    // {
+    //     $u = factory(\App\User::class)->make();
+    //     $this->actingAs($u)->get('/admin/users')->assertOk();
+    // }
+
+    /** @test */
+    public function field_names()
+    {
+        $u = factory(\App\User::class)->make();
+        $this->actingAs($u)->get('/admin/field-names')->assertOk();
+    }
+
+    /** @test */
+    public function queries()
+    {
+        $u = factory(\App\User::class)->make();
+        $this->actingAs($u)->get('/admin/queries')->assertOk();
     }
 }
