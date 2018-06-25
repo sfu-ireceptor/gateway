71020577b982172fb95b5f6036fcb7922a22d9bf
Jerome Jaglale <jerome_jaglale@sfu.ca>
2018-06-25 13:45:41 -0700

    Add script to delete old downloads

---
+++ util/delete_old_downloads.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOWNLOADS_FOLDER=${SCRIPT_FOLDER}'/../storage/app/public/'
+
+find $DOWNLOADS_FOLDER -not -path '*/\.*' -type f \( ! -iname ".*" \) -mtime +6 -delete
+
+# Reference
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+# https://askubuntu.com/questions/789602/auto-delete-files-older-than-7-days
