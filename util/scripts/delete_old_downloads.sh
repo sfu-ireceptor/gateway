ea7bf55c2f8c65ddf941bcc0281c23c225307dae
Jerome Jaglale <jerome_jaglale@sfu.ca>
2018-06-26 12:49:00 -0700

    Log crontab output

--- util/scripts/delete_old_downloads.sh
+++ util/scripts/delete_old_downloads.sh
@@ -3,6 +3,7 @@
 SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DOWNLOADS_FOLDER=${SCRIPT_FOLDER}'/../../storage/app/public/'
 
+echo "Deleting old files in $DOWNLOADS_FOLDER"
 find $DOWNLOADS_FOLDER -not -path '*/\.*' -type f \( ! -iname ".*" \) -mtime +6 -delete
 
 # Reference
