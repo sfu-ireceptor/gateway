a0aa7b2b9c0d1f2200c91e8523db9f223ede0319
Jerome Jaglale <jerome_jaglale@sfu.ca>
2018-06-26 13:06:29 -0700

    Make old files delete script work

--- util/scripts/delete_old_downloads.sh
+++ util/scripts/delete_old_downloads.sh
@@ -4,8 +4,9 @@ SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DOWNLOADS_FOLDER=${SCRIPT_FOLDER}'/../../storage/app/public/'
 
 echo "Deleting old files in $DOWNLOADS_FOLDER"
-find $DOWNLOADS_FOLDER -not -path '*/\.*' -type f \( ! -iname ".*" \) -mtime +6 -delete
+find $DOWNLOADS_FOLDER  -type f  -name '[!.]*' -mtime +6 -delete
 
 # Reference
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 # https://askubuntu.com/questions/789602/auto-delete-files-older-than-7-days
+# https://superuser.com/questions/152958/exclude-hidden-files-when-searching-with-unix-linux-find/999448#999448
