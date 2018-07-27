d2678a2b25a36bd002ed0a997e2b3b5ff330550b
Jerome Jaglale <jerome_jaglale@sfu.ca>
2018-07-27 13:28:02 -0700

    Refactor canarie routes feature test

--- tests/Feature/CanarieTest.php
+++ tests/Feature/CanarieTest.php
@@ -8,21 +8,27 @@ class CanarieTest extends TestCase
 {
     private $prefixes = ['platform', 'auth/service', 'computation/service'];
 
+    private $html_routes = ['info', 'stats', 'doc', 'releasenotes', 'support', 'source', 'tryme', 'licence', 'provenance', 'factsheet'];
+    private $json_routes = ['info', 'stats'];
+
     /** @test */
-    public function test_all_canarie_routes()
+    public function test_html_routes()
     {
         foreach ($this->prefixes as $prefix) {
-            // html
-            $routes = ['info', 'stats', 'doc', 'releasenotes', 'support', 'source', 'tryme', 'licence', 'provenance', 'factsheet'];
-            foreach ($routes as $route) {
+            foreach ($this->html_routes as $route) {
                 $this->get($prefix . '/' . $route)->assertOk();
             }
+        }
+    }
 
-            // json
-            $routes = ['info', 'stats'];
-            foreach ($routes as $route) {
+    /** @test */
+    public function test_json_routes()
+    {
+        foreach ($this->prefixes as $prefix) {
+            foreach ($this->json_routes  as $route) {
                 $this->json('GET', $prefix . '/' . $route)->assertJson([]);
             }
         }
     }
+
 }
