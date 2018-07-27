22f7c2b2c2da280fb4cba9f154f2f60e08f7954a
Jerome Jaglale <jerome_jaglale@sfu.ca>
2018-07-27 13:21:38 -0700

    Add feature test to make sure CANARIE routes are not broken

---
+++ tests/Feature/CanarieTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Support\Facades\Log;
+
+class CanarieTest extends TestCase
+{
+	private $prefixes = ['platform', 'auth/service', 'computation/service'];
+
+    /** @test */
+    public function test_all_canarie_routes()
+    {
+    	foreach ($this->prefixes as $prefix) {
+    		// html
+    		$routes = ['info', 'stats', 'doc', 'releasenotes', 'support', 'source', 'tryme', 'licence', 'provenance', 'factsheet'];
+    		foreach ($routes as $route) {
+		        $this->get($prefix . '/' . $route)->assertOk();
+    		}
+
+    		// json
+    		$routes = ['info', 'stats'];
+    		foreach ($routes as $route) {
+		        $this->json('GET', $prefix . '/' . $route)->assertJson([]);
+    		}
+    	}
+    }
+}
