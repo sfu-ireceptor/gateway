75d309368a26325f7aa6190628742475b77fcdbb
Jerome Jaglale <jerome_jaglale@sfu.ca>
2018-10-19 16:35:22 -0700

    Make stats on sequence quick search page work

--- app/Sample.php
+++ app/Sample.php
@@ -25,7 +25,7 @@ class Sample
         // generate list of sample ids
         $sample_id_list = [];
         foreach ($sample_list as $sample) {
-            $sample_id_list['ir_project_sample_id_list_' . $sample->rest_service_id][] = $sample->ir_project_sample_id;
+            $sample_id_list['ir_project_sample_id_list_' . $sample->real_rest_service_id][] = $sample->ir_project_sample_id;
         }
 
         return $sample_id_list;
