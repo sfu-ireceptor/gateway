3edb3f34b016fd9f786a65cecfe60eb5e46a97b6
Jerome Jaglale <jerome_jaglale@sfu.ca>
2018-10-31 13:59:53 -0700

    Prevent trimmimg for value submitted from "Cell subset" dropdown (IR-902)

--- app/Http/Middleware/TrimStrings.php
+++ app/Http/Middleware/TrimStrings.php
@@ -14,5 +14,26 @@ class TrimStrings extends BaseTrimmer
     protected $except = [
         'password',
         'password_confirmation',
+        'cell_subset',
     ];
+
+    // override cleanValue() and cleanArray() from TransformsRequest so array attributes can also be excluded
+    protected function cleanValue($key, $value)
+    {
+        if (is_array($value)) {
+            return $this->cleanArray($value, $key);
+        }
+
+        return $this->transform($key, $value);
+    }
+
+    protected function cleanArray(array $data, $key = '')
+    {
+        return collect($data)->map(function ($arrayValue, $arrayKey) use ($key) {
+            if ($key != '') {
+                $arrayKey = $key;
+            }
+            return $this->cleanValue($arrayKey, $arrayValue);
+        })->all();
+    }
 }
