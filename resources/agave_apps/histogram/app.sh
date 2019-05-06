c2a81603c49fb62f906af7ccbd0c69036dd33ec2
Brian Corrie <bdcorrie@gmail.com>
2019-05-06 14:57:22 -0700

    Rewrote based on app.sh from Stats AGAVE app

--- resources/agave_apps/histogram/app.sh
+++ resources/agave_apps/histogram/app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Histogram App 6.0 (2019-05-06)"
+echo "Histogram App 5.0"
 
 ##############################################
 # init environment
@@ -8,46 +8,81 @@ if [ -f /etc/bashrc ]; then
 . /etc/bashrc
 fi
 
-# app variables (will be subsituted by AGAVE)
-ZIP_FILE=${file1}
-VARNAME=${param1}
+# Load the environment/modules needed.
+module load scipy-stack
+
+# app variables (will be subsituted by AGAVE). If they don't exist
+# use command line arguments.
+if [ -z "${file1}" ]; then
+	ZIP_FILE=$1
+	VARNAME=$2
+else
+	ZIP_FILE=${file1}
+	VARNAME=${param1}
+fi
+
+function do_histogram()
+# Parameters: VARNAME to process, array of input files
+{
+    # Temporary file for data
+    TMP_FILE=tmp.tsv
+
+    # preprocess input files -> tmp.csv
+    echo "Extracting $1 from files started at: `date`" 
+    echo $1 > $TMP_FILE
+    for f in "${tsv_files[@]}"; do
+	echo "    Extracting $1 from $f"
+	python preprocess.py $f $1 >> $TMP_FILE
+    done
+
+    ##############################################
+    # Generate the image file.
+    OFILE_BASE="report-$1-histogram"
+
+    # Debugging output
+    echo "Histogram started at: `date`" 
+    echo "Input file = $TMP_FILE"
+    echo "Variable = $1"
+    echo "Output file = $OFILE_BASE.png"
+
+    # Run the python histogram command
+    #python histogram.py $TMP_FILE $OFILE
+    python airr_histogram.py $1 $TMP_FILE $OFILE_BASE.png
+    #convert $OFILE_BASE.png $OFILE_BASE.jpg
+
+    # change permissions
+    chmod 644 "$OFILE_BASE.png"
+    #chmod 644 "$OFILE_BASE.jpg"
+
+    # Remove the temporary file.
+    rm -f $TMP_FILE
+}
+
+# The Gateway provides information about the download in the file info.txt
+INFO_FILE=info.txt
 
 ##############################################
 # uncompress zip file
-unzip "$ZIP_FILE" && rm "$ZIP_FILE"
+#XXXunzip "$ZIP_FILE" && rm "$ZIP_FILE"
+echo "Extracting files started at: `date`" 
+unzip -o "$ZIP_FILE" 
 
-# preprocess input files -> tmp.csv
-echo "$VARNAME" >> tmp.csv
-for f in *.tsv; do
-	python preprocess.py "$f" "$VARNAME" >> tmp.csv
+# Determine the files to process. We extract the .tsv files from the info.txt
+tsv_files=( `cat $INFO_FILE | awk -F":" 'BEGIN {count=0} /tsv/ {if (count>0) printf(" %s",$1); else printf("%s", $1); count++}'` )
+
+# Run the historgram for the variable of interest
+do_histogram $VARNAME
+
+# Cleanup the input data files, don't want to return them as part of the resulting analysis
+echo "Removing original ZIP file $ZIP_FILE"
+rm -f $ZIP_FILE
+echo "Removing extracted files for each repository"
+for f in "${tsv_files[@]}"; do
+    echo "    Removing extracted file $f"
+    rm -f $f
 done
 
-##############################################
-# use tmp.csv to generate the histogram
-IFILE="tmp.csv"
-OFILE="histogram.jpg"
-
-# Debugging output
-echo "Histogram started at: `date`" 
-echo "Input file = $IFILE"
-echo "Variable = $VARNAME"
-echo "Output file = $OFILE"
-
-# Generate the matlab command to execute.
-COMMAND="histogram('$IFILE','$VARNAME',-1,'Frequency Count','$OFILE')"
-echo "Command = $COMMAND"
-
-# Load the module required. We need 2014a for some of the table functions.
-module load matlab/2014a
-
-# Run matlab with the command
-matlab -nosplash -nodisplay -singleCompThread -r "$COMMAND;exit"
-
-# change permissions
-chmod 644 "$OFILE"
 
 # Debugging output, print data/time when shell command is finished.
 echo "Histogram finished at: `date`"
 
-# rm *tsv
-# rm tmp.csv
