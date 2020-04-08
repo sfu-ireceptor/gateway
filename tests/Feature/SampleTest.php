b5ee53b4e6bf205257d2811bfdfb664765779efe
Jerome Jaglale <jerome_jaglale@sfu.ca>
2020-04-08 20:42:41 +0000

    Fix SampleTest

--- tests/Feature/SampleTest.php
+++ tests/Feature/SampleTest.php
@@ -8,6 +8,8 @@ use Tests\TestCase;
 
 class SampleTest extends TestCase
 {
+    protected static $repertoire_data = [];
+
     private $rs = [
         'id' => 1,
         'name' => 'Fake Rest Service',
@@ -15,7 +17,7 @@ class SampleTest extends TestCase
     ];
 
     private $sample = [
-        '_id' => 4,
+        'repertoire_id' => 4,
         'collection_time_point_relative' => 'Week 1',
         'sequencing_facility' => null,
         'cell_phenotype' => null,
@@ -101,6 +103,108 @@ class SampleTest extends TestCase
         'real_rest_service_id' => 1,
     ];
 
+    public static function setUpBeforeClass()
+    {
+        self::$repertoire_data = [
+            (object) [
+                'repertoire_id' => 8,
+                'subject' => (object) [
+                    'sex' => 'Female',
+                    'diagnosis' => (object) [
+                        'disease_length' => null,
+                        'immunogen' => null,
+                        'medical_history' => null,
+                        'intervention' => null,
+                        'disease_diagnosis' => 'Multiple Sclerosis',
+                        'study_group_description' => 'Case',
+                        'disease_stage' => null,
+                        'prior_therapies' => null,
+                    ],
+                    'linked_subjects' => null,
+                    'strain_name' => null,
+                    'race' => null,
+                    'age' => '54 years',
+                    'subject_id' => '26712_CSF',
+                    'organism' => (object) [
+                        'value' => 'Homo sapiens',
+                    ],
+                    'link_type' => null,
+                    'ethnicity' => null,
+                    'ancestry_population' => null,
+                    'synthetic' => false,
+                    'age_event' => null,
+                ],
+                'sample' => [
+                    0 => (object) [
+                        'sequencing_kit' => null,
+                        'total_reads_passing_qc_filter' => 4854,
+                        'tissue' => 'Blood',
+                        'cell_phenotype' => 'CD19, IgD, CD27',
+                        'physical_linkage' => null,
+                        'pcr_target' => [
+                            0 => (object) [
+                                'forward_pcr_primer_target_location' => 'IgM-VH, IgG-VH',
+                                'reverse_pcr_primer_target_location' => 'IgG, IgM isotype specific reverse primers',
+                                'pcr_target_locus' => 'CDR3',
+                            ],
+                        ],
+                        'cell_processing_protocol' => null,
+                        'collection_time_point_relative' => 'Time 0',
+                        'sequencing_platform' => '454 GS FLX Titanium',
+                        'cell_number' => null,
+                        'library_generation_kit_version' => null,
+                        'disease_state_sample' => 'Multiple Sclerosis',
+                        'biomaterial_provider' => null,
+                        'template_amount' => null,
+                        'sequencing_run_date' => null,
+                        'cell_subset' => 'Peripheral blood mononuclear cells',
+                        'template_class' => 'RNA',
+                        'template_quality' => null,
+                        'sample_type' => null,
+                        'library_generation_protocol' => null,
+                        'cell_quality' => null,
+                        'single_cell' => false,
+                        'anatomic_site' => null,
+                        'cell_isolation' => null,
+                        'tissue_processing' => null,
+                        'complete_sequences' => 'complete ',
+                        'read_length' => null,
+                        'collection_time_point_reference' => 'Sample collection',
+                        'sample_id' => '26712_CSF',
+                        'cells_per_reaction' => '1*10^9',
+                        'library_generation_method' => 'RT-PCR',
+                        'sequencing_run_id' => null,
+                        'cell_storage' => null,
+                        'sequencing_facility' => null,
+                    ],
+                ],
+                'study' => (object) [
+                    'lab_address' => 'University of California San Francisco (UCSF)',
+                    'study_id' => 'PRJNA248411',
+                    'grants' => null,
+                    'study_description' => 'Multiple Sclerosis Study',
+                    'inclusion_exclusion_criteria' => null,
+                    'submitted_by' => 'Palanichamy, Apeltsin et al ',
+                    'pub_ids' => 'PMC4176763',
+                    'study_title' => 'Immunoglobulin class-switched B cells provide an active immune axis between CNS and periphery in multiple sclerosis',
+                    'lab_name' => 'Von Budingen Lab',
+                    'collected_by' => 'Hans-Christian.vonBuedingen@ucsf.edu',
+                ],
+                'data_processing' => (object) [
+                    'paired_reads_assembly' => null,
+                    'primer_match_cutoffs' => null,
+                    'software_versions' => 'SRA Toolkit 2.8.2-1, cutadapt 1.14, fastqc 0.11.5, pear 0.9.10, biopython 2.7.13, igblast 1.8.0',
+                    'collapsing_method' => null,
+                    'quality_thresholds' => 30,
+                    'data_processing_protocols' => 'Barcodes and primers removed and igblast/imgt annotation of quality filtered sequences',
+                ],
+                'real_rest_service_id' => 40,
+                'ir_sequence_count' => 4846,
+                'ir_filtered_sequence_count' => 4846,
+            ],
+        ];
+    }
+
     /*
     |--------------------------------------------------------------------------
     | Simulate service returning 1 sample
@@ -115,7 +219,7 @@ class SampleTest extends TestCase
             [
                 'rs' => (object) $this->rs,
                 'status' => 'success',
-                'data' => [(object) $this->sample],
+                'data' => self::$repertoire_data,
             ],
         ];
 
