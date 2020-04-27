859748e5667b135b2a06e53192411a105da69cba
Jerome Jaglale <jerome_jaglale@sfu.ca>
2020-04-27 19:28:03 +0000

    Force CSS/JS refresh

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -13,7 +13,7 @@
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
 		<link href="/css/bootstrap-multiselect.css" rel="stylesheet" />
 		<link href="/css/jstree/default/style.min.css" rel="stylesheet" />
-		<link href="/css/main.css?v=36" rel="stylesheet">
+		<link href="/css/main.css?v=37" rel="stylesheet">
 
 		<!-- IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
@@ -181,7 +181,7 @@
 		<script src="/js/bootstrap-multiselect.js"></script>
 		<script src="/js/pluralize.js"></script>
 
-		<script src="/js/main.js?v=26"></script>
+		<script src="/js/main.js?v=27"></script>
 		<script src="/js/visualization.js?v=15"></script>
 	</body>
 
