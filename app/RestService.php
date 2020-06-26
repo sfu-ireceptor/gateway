a1a2010a08a9f0a2facdf77dc2984e10477fbe52
Jerome Jaglale <jerome_jaglale@sfu.ca>
2020-06-26 21:33:21 +0000

    Show productive individual sequences by default (IR-1987)

--- app/RestService.php
+++ app/RestService.php
@@ -598,6 +598,11 @@ class RestService extends Model
             $service_filters['repertoire_id'] = $service_filters['ir_project_sample_id_list'];
             unset($service_filters['ir_project_sample_id_list']);
 
+            // if productive filter is not set, set it to true 
+            if(! isset($service_filters['productive'])) {
+                $service_filters['productive'] = true;
+            }
+
             // if no sequence filters, query only subset of repertoires
             if (count($service_filters) == 1) {
                 $service_filters['repertoire_id'] = array_slice($service_filters['repertoire_id'], 0, 20);
