f310c42439f67ca99a5d3b72ae259d42540b957b
Jerome Jaglale <jerome.jaglale@gmail.com>
2020-06-29 20:08:04 +0000

    Apply fixes from StyleCI

--- app/RestService.php
+++ app/RestService.php
@@ -598,7 +598,6 @@ class RestService extends Model
             $service_filters['repertoire_id'] = $service_filters['ir_project_sample_id_list'];
             unset($service_filters['ir_project_sample_id_list']);
 
-
             // if no sequence filters, query only subset of repertoires
             if (count($service_filters) == 1) {
                 $service_filters['repertoire_id'] = array_slice($service_filters['repertoire_id'], 0, 20);
