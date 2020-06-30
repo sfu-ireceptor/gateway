aab508ccd1462c6153c25068c1137f34140b93c9
Jerome Jaglale <jerome_jaglale@sfu.ca>
2020-06-30 21:40:45 +0000

    Reformat news page, add link to Twitter

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -6,19 +6,32 @@
 <div class="container page">
 
 	<h1>What's New</h1>
-
-	<div class="row news_list">
-		<div class="col-md-6">
+	<div class="row">
+		<div class="col-md-7">
 			@foreach ($news_list as $n)
-				<div class="panel panel-default beta_version2	">
-					<div class="panel-heading">
-						<h3 class="panel-title">{{ Carbon\Carbon::parse($n->created_at)->format('M d, Y') }}</h3>							
-					</div>
-					<div class="panel-body">
-						{!! $n->message !!}
-					</div>
+			<div class="row news_item">
+				<div class="col-md-12">
+					<p class="news_date">{{ Carbon\Carbon::parse($n->created_at)->format('M d, Y') }}</p>						
+					{!! $n->message !!}
 				</div>
+			</div>
 			@endforeach
+		</div>
+		<div class="col-md-1">		
+		</div>
+		<div class="col-md-4">		
+			<div class="panel panel-default">
+			  <div class="panel-heading">
+			    <h3 class="panel-title">Twitter</h3>
+			  </div>
+			  <div class="panel-body">
+				You can 
+				<a href="https://twitter.com/ireceptorgw">
+					follow us on Twitter 
+				</a>
+				to keep up with the latest news.
+			  </div>
+			</div>
 		</div>
 	</div>
 
