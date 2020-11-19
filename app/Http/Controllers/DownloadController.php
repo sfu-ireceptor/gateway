d6a94d3f39f3078b882e0502786d6dbf076f98e5
jeromejaglale <jerome.jaglale@gmail.com>
2020-11-19 23:42:37 +0000

    Add "Send to Galaxy" button

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -4,16 +4,39 @@ namespace App\Http\Controllers;
 
 use App\Bookmark;
 use App\Download;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class DownloadController extends Controller
 {
-    public function getIndex()
+    public function getIndex(Request $request)
     {
-        $username = auth()->user()->username;
+        $user = auth()->user();
+
+        // if there are galaxy parameters, save them and redirect
+        if ($request->has('GALAXY_URL') && $request->has('tool_id')) {
+            $galaxy_url = $request->input('GALAXY_URL');
+            $tool_id = $request->input('tool_id');
+
+            $user->galaxy_url = $galaxy_url;
+            $user->galaxy_tool_id = $tool_id;
+            $user->save();    
+            
+            return redirect('downloads')->with('notification', 'You can now send your downloads to Galaxy with the "Send to Galaxy" buttons.');        
+        }
+
+        $username = $user->username;
         $download_list = Download::where('hidden', false)->where('username', '=', $username)->orderBy('id', 'desc')->get();
 
         $data = [];
         $data['download_list'] = $download_list;
+
+        // if galaxy is enabled
+        if(isset($user->galaxy_url)) {
+            $data['galaxy_enabled'] = true;
+            $data['galaxy_tool_id'] = $user->galaxy_tool_id;
+            $data['galaxy_url'] = $user->galaxy_url;
+        }
 
         return view('downloadList', $data);
     }
