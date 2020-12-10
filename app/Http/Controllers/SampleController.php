b69fc0bd97cc06ddfce52e88bae86c7b5733efbe
jeromejaglale <jerome.jaglale@gmail.com>
2020-12-10 21:29:30 +0000

    Force JSON file download (IR-2220)

--- app/Http/Controllers/SampleController.php
+++ app/Http/Controllers/SampleController.php
@@ -449,7 +449,7 @@ class SampleController extends Controller
             $query_log->save();
         }
 
-        return redirect($file_path);
+        return response()->download($file_path);
     }
 
     public function tsv(Request $request)
@@ -476,7 +476,7 @@ class SampleController extends Controller
             $query_log->save();
         }
 
-        return redirect($file_path);
+        return response()->download($file_path);
     }
 
     public function field($id)
