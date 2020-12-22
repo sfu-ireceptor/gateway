29153a22f8b815b32b8cdd9b03ca5cea72743245
jeromejaglale <jerome.jaglale@gmail.com>
2020-12-22 22:43:14 +0000

    Fix typo

--- resources/views/allDownloads.blade.php
+++ resources/views/allDownloads.blade.php
@@ -58,7 +58,7 @@
 						<span class="label label-success">Finished</span>
 
 						@if($d->isIncomplete())
-							<span class="help help_queue_position text-danger" role="button" data-container="body" data-toggle="popover_form_field" data-placement="right" title="Download incomplete" data-content="<p>See the file info.txt for details.</p>" data-trigger="hover" tabindex="0">
+							<span class="help help_queue_position text-danger" role="button" data-container="body" data-toggle="popover_form_field" data-placement="right" title="Download is incomplete" data-content="<p>See the file info.txt for details.</p>" data-trigger="hover" tabindex="0">
 								<span class="glyphicon glyphicon-warning-sign"></span>
 							</span>
 						@endif
