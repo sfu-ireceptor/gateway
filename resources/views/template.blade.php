b96b28362e10d3a7a88db95b7c8473c4cb61c0e3
jeromejaglale <jerome.jaglale@gmail.com>
2021-01-25 23:16:13 +0000

    Update AIRR vis library to latest version

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -200,7 +200,7 @@
 		<script src="/js/bootstrap-multiselect.js"></script>
 		<script src="/js/pluralize.js"></script>
 
-		<script src="/js/airrvisualization.js?v=5"></script>
+		<script src="/js/airrvisualization.js?v=6"></script>
 		<script src="/js/main.js?v=44"></script>
 		<script src="/js/visualization.js?v=17"></script>
 	</body>
