5bc7103ee0da887fef415bc567760e29da782c6d
jeromejaglale <jerome.jaglale@gmail.com>
2021-02-11 00:18:36 +0000

    Upgrade AIRR vis library to last version

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -200,7 +200,7 @@
 		<script src="/js/bootstrap-multiselect.js"></script>
 		<script src="/js/pluralize.js"></script>
 
-		<script src="/js/airrvisualization.js?v=8"></script>
+		<script src="/js/airrvisualization.js?v=9"></script>
 		<script src="/js/main.js?v=51"></script>
 		<script src="/js/visualization.js?v=19"></script>
 	</body>
