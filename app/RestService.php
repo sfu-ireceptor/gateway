186b0ed72bfd055a7ce921398313387d665a9440
jeromejaglale <jerome.jaglale@gmail.com>
2021-03-01 22:07:01 +0000

    Trigger sequence download only if expected nb of sequences > 0

--- app/RestService.php
+++ app/RestService.php
@@ -961,7 +961,7 @@ class RestService extends Model
         // build list of services to query
         $rs_list = [];
         foreach (self::findEnabled() as $rs) {
-            if (isset($expected_nb_sequences_by_rs[$rs->id])) {
+            if (isset($expected_nb_sequences_by_rs[$rs->id]) && ($expected_nb_sequences_by_rs[$rs->id] > 0)) {
                 $rs_list[$rs->id] = $rs;
             }
         }
