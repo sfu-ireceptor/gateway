28e19b7cf35afbec999d925ac4c41ca0c5f159c5
jeromejaglale <jerome.jaglale@gmail.com>
2021-03-22 21:05:49 +0000

    Add a few fake users

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -16,6 +16,30 @@ class UserSeeder extends Seeder
                 'email' => 'mercury@solarsystem.uni',
                 'admin' => true,
             ],
+            [
+                'username' => 'venus',
+                'password' => 'tN4ebeaeETVPf6gg',
+                'first_name' => 'Venus',
+                'last_name' => 'Planet',
+                'email' => 'venus@solarsystem.uni',
+                'admin' => true,
+            ],
+            [
+                'username' => 'earth',
+                'password' => 'tN4ebeaeETVPf6gg',
+                'first_name' => 'Earth',
+                'last_name' => 'Planet',
+                'email' => 'earth@solarsystem.uni',
+                'admin' => true,
+            ],
+            [
+                'username' => 'mars',
+                'password' => 'tN4ebeaeETVPf6gg',
+                'first_name' => 'Mars',
+                'last_name' => 'Planet',
+                'email' => 'mars@solarsystem.uni',
+                'admin' => true,
+            ],
         ];
 
         foreach ($l as $t) {
