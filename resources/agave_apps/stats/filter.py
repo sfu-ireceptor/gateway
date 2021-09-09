58d53155df7c7976f23fea8818e00d4cbf234a54
bcorrie <bdcorrie@gmail.com>
2021-09-09 22:47:37 +0000

    filter.py now takes a command line argument for the output file.

--- resources/agave_apps/stats/filter.py
+++ resources/agave_apps/stats/filter.py
@@ -4,6 +4,7 @@ import pandas
 filename = sys.argv[1]
 field_name = sys.argv[2]
 field_value = sys.argv[3]
+outfile = sys.argv[4]
 chunk_size = 100000
 
 airr_df_reader = pandas.read_csv(filename, sep='\t', chunksize=chunk_size, dtype= {'repertoire_id': str})
@@ -15,9 +16,9 @@ for airr_df in airr_df_reader:
         field_df = airr_df.iloc[list(airr_df[field_name] == str(field_value)), :]
         print("Writing %d records"%(field_df.index.size))
         if chunk_count == 0:
-            field_df.to_csv(field_value+"_"+filename, sep='\t', mode='a', header=True, index=False)
+            field_df.to_csv(outfile, sep='\t', mode='a', header=True, index=False)
         else:
-            field_df.to_csv(field_value+"_"+filename, sep='\t', mode='a', header=False, index=False)
+            field_df.to_csv(outfile, sep='\t', mode='a', header=False, index=False)
         chunk_count = chunk_count + 1
         total_size = total_size + field_df.index.size
 
