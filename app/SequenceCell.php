2db7b3323b042d1d8b0734fdd495d556241cf768
jeromejaglale <jerome.jaglale@gmail.com>
2022-04-21 20:39:57 +0000

    Display cell fields on cells page (IR-2630)

--- app/SequenceCell.php
+++ app/SequenceCell.php
@@ -50,7 +50,7 @@ class SequenceCell
 
         // convert any array properties to strings
         // $cell_list = array_map('convert_arrays_to_strings', $cell_list);
-        $cell_list = FieldName::convertObjectList($cell_list, 'ir_adc_api_query', 'ir_id');
+        $cell_list = FieldName::convertObjectList($cell_list, 'ir_adc_api_query', 'ir_id', 'Cell');
 
         // add to stats data
         $data['items'] = $cell_list;
