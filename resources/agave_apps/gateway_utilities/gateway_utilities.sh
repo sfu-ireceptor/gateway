1c52a10f2a97ca749a51c9b5fb96da1382fc9968
bcorrie <bdcorrie@gmail.com>
2022-05-04 21:25:33 +0000

    Major refactor
    Changed it so that the gateway utilities can handle different types
    of data in the analyses
    
    Also added proper use of the manifest to determine which files to use
    from which repositories.

--- resources/agave_apps/gateway_utilities/gateway_utilities.sh
+++ resources/agave_apps/gateway_utilities/gateway_utilities.sh
@@ -41,16 +41,25 @@ function gateway_split_repertoire(){
 #     $2 - AIRR Manifest file
 #     $3 - iReceptor ZIP file
 #     $4 - Working directory
+#     $5 - Type of analysis from the manifest (rearrangement_file, clone_file, cell_file)
 
     # The Gateway provides information about the download in the file info.txt and
     # an AIRR Manifest JSON file.
-    INFO_FILE=$1
-    MANIFEST_FILE=$2
+    local INFO_FILE=$1
+    local MANIFEST_FILE=$2
     # The data, including the info and manifest files, are in the ZIP file.
-    ZIP_FILE=$3
+    local ZIP_FILE=$3
     # We want a working directory for the processing
-    WORKING_DIR=$4
-    
+    local WORKING_DIR=$4
+    # The type of analysis to do
+    local ANALYSIS_TYPE=""
+    if [ -z "$5" ]; then
+        ANALYSIS_TYPE="rearrangement_file"
+    else
+        ANALYSIS_TYPE=$5
+    fi
+
+
     # Unzip the iReceptor Gateway ZIP file into the working directory
     gateway_unzip ${ZIP_FILE} ${WORKING_DIR}
 
@@ -60,61 +69,141 @@ function gateway_split_repertoire(){
     # Move into the working directory to do work...
     pushd ${WORKING_DIR} > /dev/null
 
-    # Determine the files to process. We extract the .tsv files from the info.txt
-    # and store them in an array.
-    tsv_files=( `python3 ${SCRIPT_DIR}/${GATEWAY_UTIL_DIR}/manifest_summary.py ${MANIFEST_FILE} rearrangement_file` )
+    # Determine the files to process. We extract the data files from the airr_manifest.json
+    # and store them in an array. The type is one of rearrangement_file, cell_file, clone_file
+    data_files=( `python3 ${SCRIPT_DIR}/${GATEWAY_UTIL_DIR}/manifest_summary.py ${MANIFEST_FILE} ${ANALYSIS_TYPE}` )
     if [ $? -ne 0 ]
     then
-	echo "IR-ERROR: Could not process manifest file ${MANIFEST_FILE}"
-	exit $?
+        echo "IR-ERROR: Could not process manifest file ${MANIFEST_FILE}"
+        exit $?
     fi
-    echo "TSV files = ${tsv_files}"
+    echo "Data files = ${data_files[@]}"
+    if [ ${#data_files[@]} -eq 0 ]; then
+        echo "IR-INFO: Could not find any ${ANALYSIS_TYPE} in ${MANIFEST_FILE}"
+    fi
 
-    # For each TSV file in the array, process it.
-    for f in "${tsv_files[@]}"; do
-	# Get an array of unique repertoire_ids from the TSV file
-        echo "    Extracting ${SPLIT_FIELD} from $f"
-        repertoire_ids=( `python3 ${SCRIPT_DIR}/${GATEWAY_UTIL_DIR}/preprocess.py $f $SPLIT_FIELD | sort -u | awk '{printf("%s ",$0)}'` )
+    # Get the repository from the manifest file.
+    repository_urls=( `python3 ${SCRIPT_DIR}/${GATEWAY_UTIL_DIR}/manifest_summary.py ${MANIFEST_FILE} "repository"` )
+    echo "Repository URLs = ${repository_urls[@]}"
 
-	# Create a directory for each repository (mkdir if it isn't already with -p)
-        repository_name="${f%.*}"
+    # Get the Reperotire files from the manifest file.
+    repertoire_files=( `python3 ${SCRIPT_DIR}/${GATEWAY_UTIL_DIR}/manifest_summary.py ${MANIFEST_FILE} "repertoire_file"` )
+    echo "Repertoire files = ${repertoire_files[@]}"
+
+    # For each repository, process the data from it.
+    count=0
+    for repository_url in "${repository_urls[@]}"; do
+        data_file=${data_files[$count]}
+        repertoire_file=${repertoire_files[$count]}
+        repository_name=`echo "$repository_url" | awk -F/ '{print $3}'`
+        echo ""
+        echo "Processing data from repository ${repository_name}"
+        echo "Repertoire file = ${repertoire_file}"
+        echo "Data file = ${data_file}"
+
+        # Get an array of unique repertoire_ids from the data file
+        echo "    Extracting ${SPLIT_FIELD} from $data_file"
+        if [ ${ANALYSIS_TYPE} = "rearrangement_file" ]
+        then
+            # preprocess.py dumps a field of interest from a TSV data file. We want
+            # all of the reperotire_ids - we sort them to get unique ids and then
+            # use awk to print them all on the same line to create an array
+            # of repertoire_ids
+            repertoire_ids=( `python3 ${SCRIPT_DIR}/${GATEWAY_UTIL_DIR}/preprocess.py $data_file $SPLIT_FIELD | sort -u | awk '{printf("%s ",$0)}'` )
+        elif [ ${ANALYSIS_TYPE} = "cell_file" ]
+        then
+            # preprocess-json.py dumps a field of interest from a JSON data file. We want
+            # all of the reperotire_ids - we sort them to get unique ids and then
+            # use awk to print them all on the same line to create an array
+            # of repertoire_ids
+            repertoire_ids=( `python3 ${SCRIPT_DIR}/${GATEWAY_UTIL_DIR}/preprocess-json.py $data_file Cell $SPLIT_FIELD | sort -u | awk '{printf("%s ",$0)}'` )
+        elif [ ${ANALYSIS_TYPE} = "clone_file" ]
+        then
+            # preprocess-json.py dumps a field of interest from a JSON data file. We want
+            # all of the reperotire_ids - we sort them to get unique ids and then
+            # use awk to print them all on the same line to create an array
+            # of repertoire_ids
+            repertoire_ids=( `python3 ${SCRIPT_DIR}/${GATEWAY_UTIL_DIR}/preprocess-json.py $data_file Clone $SPLIT_FIELD | sort -u | awk '{printf("%s ",$0)}'` )
+        else
+            echo "IR-ERROR: Do not know how to split repertoires for ${ANALYSIS_TYPE}"
+            exit 1
+        fi
+
+        # Create a directory for each repository (mkdir if it isn't already with -p)
         mkdir -p ${repository_name}
-	# Get the accompanying JSON file for the repository.
-        json_file=${repository_name}-metadata.json
-        echo "JSON file = ${json_file}"
-	
-	# For each repertoire_id, extract the data in a directory for that repertoire
+        
+        # For each repertoire_id, extract the data in a directory for that repertoire
         for repertoire_id in "${repertoire_ids[@]}"; do
-	    # Get a directory name and make the directory
-            echo "File $f has repertoire_id = ${repertoire_id}"
-	    repertoire_dirname=${repertoire_id}
-	    mkdir -p ${repository_name}/${repertoire_dirname}
+            # Get a directory name and make the directory
+            echo "File $data_file has repertoire_id = ${repertoire_id}"
+            repertoire_dirname=${repertoire_id}
+            mkdir -p ${repository_name}/${repertoire_dirname}
 
-	    # Generate a file name for the TSV data for the repertoire.
-	    repertoire_tsvfile=${repertoire_dirname}".tsv"
+            if [ ${ANALYSIS_TYPE} = "rearrangement_file" ]
+            then
+                # Generate a file name for the TSV data for the repertoire.
+                repertoire_datafile=${repertoire_dirname}".tsv"
     
-            # Filter the input file $f and extract all records that have the given
-	    # repertoire_id in the SPLIT_FIELD.
-	    # Command line parameters: inputfile, field_name, field_value, outfile
-	    python3 ${SCRIPT_DIR}/${GATEWAY_UTIL_DIR}/filter.py $f ${SPLIT_FIELD} ${repertoire_id} ${repository_name}/${repertoire_dirname}/${repertoire_tsvfile}
-	
-	    # Call the client supplied "run_analysis" callback function. Parameters:
-            #     $1 output directory
-	    #     $2 repository name
-	    #     $3 repertoire id ("NULL" if not used)
-            #     $4 repertoire JSON file ["NULL" if not used, required if repertoire_id is specified]
-            #     $5-$N list of TSV input files
-	    tsv_array=( "${repository_name}/${repertoire_dirname}/${repertoire_tsvfile}" )
-	    run_analysis ${repository_name}/${repertoire_dirname} ${repository_name} ${repertoire_id} ${json_file} ${tsv_array[@]} 
+                # Filter the input file $data_file and extract all records that have the given
+                # repertoire_id in the SPLIT_FIELD.
+                # Command line parameters: inputfile, field_name, field_value, outfile
+                python3 ${SCRIPT_DIR}/${GATEWAY_UTIL_DIR}/filter.py $data_file ${SPLIT_FIELD} ${repertoire_id} ${repository_name}/${repertoire_dirname}/${repertoire_datafile}
+        
+                # Call the client supplied "run_analysis" callback function. Parameters:
+                #     $1 output directory
+                #     $2 repository name
+                #     $3 repertoire id ("NULL" if not used)
+                #     $4 repertoire JSON file ["NULL" if not used, required if repertoire_id is specified]
+                #     $5-$N list of data input files
+                data_array=( "${repository_name}/${repertoire_dirname}/${repertoire_datafile}" )
+                run_analysis ${repository_name}/${repertoire_dirname} ${repository_name} ${repertoire_id} ${repertoire_file} ${data_array[@]} 
+            elif [ ${ANALYSIS_TYPE} = "clone_file" ]
+            then
+                # Generate a file name for the TSV data for the repertoire.
+                repertoire_datafile=${repertoire_dirname}".json"
+    
+                # Filter the input file $data_file and extract all records that have the given
+                # repertoire_id in the SPLIT_FIELD.
+                # Command line parameters: inputfile, field_name, field_value, outfile
+                python3 ${SCRIPT_DIR}/${GATEWAY_UTIL_DIR}/filter-json.py $data_file Clone ${SPLIT_FIELD} ${repertoire_id} > ${repository_name}/${repertoire_dirname}/${repertoire_datafile}
+        
+                # Call the client supplied "run_analysis" callback function. Parameters:
+                #     $1 output directory
+                #     $2 repository name
+                #     $3 repertoire id ("NULL" if not used)
+                #     $4 repertoire JSON file ["NULL" if not used, required if repertoire_id is specified]
+                #     $5-$N list of data input files
+                data_array=( "${repository_name}/${repertoire_dirname}/${repertoire_datafile}" )
+                run_analysis ${repository_name}/${repertoire_dirname} ${repository_name} ${repertoire_id} ${repertoire_file} ${data_array[@]} 
+            elif [ ${ANALYSIS_TYPE} = "cell_file" ]
+            then
+                # Generate a file name for the data for the repertoire.
+                repertoire_datafile=${repertoire_dirname}".json"
+    
+                # Filter the input file $data_file and extract all records that have the given
+                # repertoire_id in the SPLIT_FIELD.
+                # Command line parameters: inputfile, field_name, field_value, outfile
+                python3 ${SCRIPT_DIR}/${GATEWAY_UTIL_DIR}/filter-json.py $data_file Cell ${SPLIT_FIELD} ${repertoire_id} > ${repository_name}/${repertoire_dirname}/${repertoire_datafile}
+        
+                # Call the client supplied "run_analysis" callback function. Parameters:
+                #     $1 output directory
+                #     $2 repository name
+                #     $3 repertoire id ("NULL" if not used)
+                #     $4 repertoire JSON file ["NULL" if not used, required if repertoire_id is specified]
+                #     $5-$N list of data input files
+                data_array=( "${repository_name}/${repertoire_dirname}/${repertoire_datafile}" )
+                run_cell_analysis ${repository_name}/${repertoire_dirname} ${repository_name} ${repertoire_id} ${repertoire_file} ${data_array[@]} 
+            fi
 
         done
+        count=$((count+1))
     done
 
     # Clean up the files we created
     # First the ZIP file
     rm -f ${ZIP_FILE}
     # Remove any TSV files extracted from the ZIP - they are big and can be re-generated
-    for f in "${tsv_files[@]}"; do
+    for f in "${data_files[@]}"; do
         rm -f $f
     done
 
