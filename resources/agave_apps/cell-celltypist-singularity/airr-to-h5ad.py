bddf52044596e1c22681cc1fc7c6aa837dac48c3
bcorrie <bdcorrie@gmail.com>
2022-08-08 02:03:11 +0000

    Major refactor - use indexing rather than slicing.
    Slicing implies copy which is horribly inefficient. Much faster now
    7s where previously took 63s to process data. Code works but needs
    cleanup.

--- resources/agave_apps/cell-celltypist-singularity/airr-to-h5ad.py
+++ resources/agave_apps/cell-celltypist-singularity/airr-to-h5ad.py
@@ -1,3 +1,4 @@
+import string
 import sys
 import argparse
 import json
@@ -73,6 +74,20 @@ def createAnnData(cell_dict_array, field, value):
     # print(adata.to_df())
     return adata
 
+def skipWhite(str_buffer, str_loc):
+    while str_buffer[str_loc] in string.whitespace:
+        str_loc = str_loc + 1
+    return str_loc
+
+def mystrip(input_string):
+    # Strip of any white space 
+    count = 0
+    for character in input_string:
+        if not character in string.whitespace:
+            break
+        count = count + 1
+    return input_string[count:]
+
 def generateH5AD(gex_filename, block, field, value):
     # Keep track of the number of properties
     count = 0
@@ -80,49 +95,78 @@ def generateH5AD(gex_filename, block, field, value):
     adata_array = []
     # Open the JSON file.
     try:
-        buffer_size = 5000000
+        buffer_size = 1*1024*1024*1024
+        #buffer_size = 5*1024*1024
+        slow = False
         with open(gex_filename, 'r') as f:
 
             # Read in our first buffer_size characters.
-            json_str = f.read(buffer_size)
+            t_start = time.perf_counter()
+            json_buffer = f.read(buffer_size)
+            json_loc = 0
+            json_str = json_buffer
+            print(id(json_buffer))
+            print(id(json_str))
 
+            count = 0
+            for character in json_str:
+                if not character in string.whitespace:
+                    break
+                count = count + 1
+            my_str = json_str[count:]
+            print(id(my_str))
             # Strip of any white space in preparation for processing.
-            json_str = json_str.strip()
-            json_len = len(json_str)
+            #json_str = json_str.lstrip()
+            json_str = mystrip(json_str) if slow == False else json_str.lstrip()
+            print(id(json_str))
+            json_loc = skipWhite(json_buffer, json_loc)
 
             # AIRR JSON files are JSON objects
-            if json_str[0] != '{':
-                print('ERROR: JSON file %s has no opening {'%(gex_filename))
+            #if json_str[0] != '{':
+            if json_buffer[json_loc] != '{':
+                print('IR_ERROR: JSON file %s has no opening {'%(gex_filename))
                 sys.exit(1)
             # Look at the next part of the string, excluding white space.
-            json_str = json_str[1:].strip()
+            #json_str = json_str[1:].lstrip()
+            #json_str = mystrip(json_str[1:]) if slow == False else json_str[1:].lstrip()
+            json_loc = skipWhite(json_buffer, json_loc+1)
 
             # We look for the block key in the string (enclosed in quotes of course)
             # If we can't find it in the first buffer_size characters we assume it isn't there.
             # We are cheating here - as we assume everything between the opening { for
             # the object and the block key we are interested in is correct JSON.
-            block_loc = json_str.find('"%s"'%(block))
+            #block_loc = json_str.find('"%s"'%(block))
+            json_loc = json_buffer.find('"%s"'%(block), json_loc)
 
-            if block_loc == -1:
-                print('ERROR: Could not find key "%s" in first %d characters of JSON file %s'%(block, buffer_size, gex_filename))
+            #if block_loc == -1:
+            if json_loc == -1:
+                print('IR_ERROR: Could not find key "%s" in first %d characters of JSON file %s'%(block, buffer_size, gex_filename))
                 sys.exit(1)
 
             # Strip off the block key and any new white space
-            json_str = json_str[block_loc+len(block)+2:].strip()
+            #json_str = json_str[block_loc+len(block)+2:].lstrip()
+            #json_str = mystrip(json_str[block_loc+len(block)+2:]) if slow == False else json_str[block_loc+len(block)+2:].lstrip()
+            json_loc = skipWhite(json_buffer, json_loc+len(block)+2)
 
             # The key should be followed by a separator :
-            if json_str[0] != ':':
-                print('ERROR: expected JSON : separator')
+            #if json_str[0] != ':':
+            if json_buffer[json_loc] != ':':
+                print('IR_ERROR: expected JSON : separator')
                 sys.exit(1)
             # Strip off the array character and any white space.
-            json_str = json_str[1:].strip()
+            #json_str = json_str[1:].lstrip()
+            #json_str = mystrip(json_str[1:]) if slow == False else json_str[1:].lstrip()
+            json_loc = skipWhite(json_buffer, json_loc+1)
 
             # The block should be an array, so we expect the array character.
-            if json_str[0] != '[':
-                print('ERROR: JSON object %s is not an array'%(block))
+            #if json_str[0] != '[':
+            if json_buffer[json_loc] != '[':
+                print('IR_ERROR: JSON object %s is not an array'%(block))
                 sys.exit(1)
             # Strip off the array character and any white space.
-            json_str = json_str[1:].strip()
+            #json_str = json_str[1:].lstrip()
+            #json_str = mystrip(json_str[1:]) if slow == False else json_str[1:].lstrip()
+            json_loc = skipWhite(json_buffer, json_loc+1)
 
             # Initalize our loop variables. We track whether we are still processing objects,
             # the maximum size of the objects in the array we are processing, the threshold that
@@ -138,27 +182,35 @@ def generateH5AD(gex_filename, block, field, value):
                 # Check to see if the buffere is almost consumed. If we have room
                 # for less than N objects, then read in more data. Don't read any more
                 # data if the file is empty.
-                buffer_remaining = len(json_str)
+                #buffer_remaining = len(json_str)
+                buffer_remaining = buffer_size - json_loc
                 if buffer_remaining < object_threshold*object_str_size and not file_empty:
                     #print('we are almost out of buffer')
                     #print('size = %d, remaing = %d, obj_size = %s'%(buffer_size,buffer_remaining,object_str_size))
                     #print('length of array = %d'%(len(cell_array)))
 
                     adata_array.append(createAnnData(cell_array, field, value))
-                    print('property count = %d, array length = %d'%(count,len(adata_array)))
+                    t_end = time.perf_counter()
+                    print('IR-INFO: property count = %d, array length = %d (%d s)'%
+                         (count,len(adata_array),t_end-t_start))
 
                     cell_array = []
                     # Read in the new data
+                    t_start = time.perf_counter()
                     new_buffer = f.read(buffer_size)
                     # If we are at file end, note it, otherwise add it on to the json string
                     if len(new_buffer) == 0:
                         file_empty = True
                     else:
-                        json_str = json_str + new_buffer
+                        #json_str = json_str + new_buffer
+                        json_buffer = json_buffer[json_loc:] + new_buffer
+                        json_loc = 0
+                        #print(json_buffer)
                 # Check to make sure that the first charater is an object delimeter. We have run
                 # strip to get rid of any whitespace so anything else is an error.
-                if json_str[0] != '{':
-                    print('ERROR: JSON object expected'%(block))
+                #if json_str[0] != '{':
+                if json_buffer[json_loc] != '{':
+                    print('IR-ERROR: JSON object expected'%(block))
                     sys.exit(1)
 
                 # Initialize our nesting level and the location in the string.
@@ -166,13 +218,17 @@ def generateH5AD(gex_filename, block, field, value):
                 location = 0
 
                 # Loop over the characters in the string.
-                for character in json_str:
+                #for character in json_str:
+                object_start = json_loc
+                while json_loc < buffer_size:
                     # For every { increase nesting, } decrease nesting
                     # The first character should be a { so nesting should start
                     # at 1 after this block
-                    if character == '{':
+                    #if character == '{':
+                    if json_buffer[json_loc] == '{':
                         nesting = nesting+1
-                    elif character == '}':
+                    #elif character == '}':
+                    elif json_buffer[json_loc] == '}':
                         nesting = nesting-1
 
                     # When nesting is 0 we have found as many } as we see {. This means
@@ -180,9 +236,13 @@ def generateH5AD(gex_filename, block, field, value):
                     if nesting == 0:
                         # Convert the string starting at 0 to location+1. Location is the
                         # location of the } character in this case.
-                        json_dict = json.loads(json_str[:location+1])
+                        #json_dict = json.loads(json_str[:location+1])
+                        object_string = json_buffer[object_start:json_loc+1]
+                        #print(object_string)
+                        json_dict = json.loads(object_string)
                         # Keep track of the larges object size to help with buffer management
-                        str_len = len(json_str[:location+1])
+                        #str_len = len(json_str[:location+1])
+                        str_len = len(object_string)
                         if str_len > object_str_size:
                             object_str_size = str_len
 
@@ -199,52 +259,62 @@ def generateH5AD(gex_filename, block, field, value):
                                 count = count + 1
                         # Strip off the object we just processed from the buffer as
                         # well as whitespace
-                        json_str = json_str[location+1:].strip()
+                        #json_str = json_str[location+1:].lstrip()
+                        #json_str = mystrip(json_str[location+1:]) if slow == False else json_str[location+1:].lstrip()
+                        json_loc = skipWhite(json_buffer, json_loc+1)
+                        
 
                         # Check if the next character is the array end character. If so we are
                         # done.
-                        if json_str[0] == ']':
+                        # if json_str[0] == ']':
+                        if json_buffer[json_loc] == ']':
                             # Strip off the ] and whitespace
-                            json_str = json_str[1:].strip()
+                            #json_str = mystrip(json_str[1:]) if slow == False else json_str[1:].lstrip()
+                            json_loc = skipWhite(json_buffer, json_loc+1)
                             # Check to see if we have a closing object character. If not
                             # things are not well formed JSON
-                            if json_str[0] != '}':
-                                print('ERROR: JSON close object expected')
+                            #if json_str[0] != '}':
+                            if json_buffer[json_loc] != '}':
+                                print('IR-ERROR: JSON close object expected')
                                 sys.exit(1)
 
                             # Keep track of the fact that we are done (exit the loop).
                             processing_object = False
                             # Break out of the buffer character processing loop.
                             break
-                        elif json_str[0] != ',':
+                        #elif json_str[0] != ',':
+                        elif json_buffer[json_loc] != ',':
                             # If the current character isn't a ] it must be a , separator
-                            print('ERROR: JSON object separator expected')
+                            print('IR-ERROR: JSON object separator expected')
                             sys.exit(1)
 
                         # If we get here we have another object to process. We strip off
                         # the , separator. The next character should therefore be a {
                         # to start the new object, which is what we want.
-                        json_str = json_str[1:].strip()
+                        #json_str = json_str[1:].lstrip()
+                        #json_str = mystrip(json_str[1:]) if slow == False else json_str[1:].lstrip()
+                        json_loc = skipWhite(json_buffer, json_loc+1)
                         # Finally break out of the character processing loop, as we want
                         # to start over with the new object processing with the current
                         # json_string in the buffer.
                         break
                     else:
                         # Keep track of where we are in the string
-                        location = location + 1
+                        #location = location + 1
+                        json_loc = json_loc + 1
         # We are done. Check to see if we have any data in the last cell_array,
         # if so create an anndata object for it and append it to our array
         if len(cell_array) > 0:
-            print('Adding %d at end of file processing'%(len(cell_array)))
+            print('IR-INFO: Adding %d at end of file processing'%(len(cell_array)))
             adata_array.append(createAnnData(cell_array, field, value))
 
         ad_concat = anndata.concat(adata_array, join='outer', merge='first',  label='concat_dataset')
         # WARNING - THIS IS INCORRECT - we need to merge cells if they have the same name
         # NOT rename them!!! DOING THIS FOR DEBUGGING ONLY
         ad_concat.obs_names_make_unique()
-        print('Length of adata_array = %d'%(len(adata_array)))
+        print('IR-INFO: Length of adata_array = %d'%(len(adata_array)))
         #print(adata_array)
-        print('Number of properties = %s'%(count))
+        print('IR-INFO: Number of properties = %s'%(count))
         print(ad_concat.to_df())
         #print(ad_concat.obs['cell_id'])
         #print(ad_concat.obs['field'])
@@ -313,6 +383,7 @@ def getArguments():
 
 
 if __name__ == "__main__":
+    total_start = time.perf_counter()
     # Get the command line arguments.
     options = getArguments()
 
@@ -338,4 +409,10 @@ if __name__ == "__main__":
         adata.uns["log1p"]["base"]=numpy.e
 
     # Write the output to the output file.
+    print("IR-INFO: Writing file %s"%(options.output_file))
     adata.write(options.output_file)
+    print("IR-INFO: Done writing file %s"%(options.output_file))
+    total_end = time.perf_counter()
+    print('IR-INFO: Total time = %d s'%(total_end-total_start))
+
+    sys.exit(0)
