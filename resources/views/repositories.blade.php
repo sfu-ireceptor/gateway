74e20e5e3bab01a58889df8c2b8060a887ff8852
jeromejaglale <jerome.jaglale@gmail.com>
2022-08-17 23:51:12 +0000

    Redesign public repositories page

--- resources/views/repositories.blade.php
+++ resources/views/repositories.blade.php
@@ -16,12 +16,65 @@
  -->
 
 	<!-- <h1><a href="https://www.antibodysociety.org/the-airr-community/airr-data-commons/">AIRR Data Commons</a> repositories queried by the iReceptor Gateway</h1> -->
-	<h1>AIRR Data Commons repositories queried by the iReceptor Gateway</h1>
+	<h1>iReceptor Gateway repositories</h1>
 
-<!-- 	<p>
-		List of the <a href="https://www.antibodysociety.org/the-airr-community/airr-data-commons/">AIRR Data Commons</a> repositories which can be browsed through the iReceptor Gateway.
+	<p class="repositories_intro">
+		Data shown on the iReceptor Gateway is retrieved in real time from these <a href="https://www.antibodysociety.org/the-airr-community/airr-data-commons/">AIRR Data Commons</a> repositories,<br />using the <a href="https://docs.airr-community.org/en/stable/api/adc_api.html">AIRR Data Commons API</a>. 
 	</p>
+
+
+<div class="row">
+
+@for ($i = 1; $i <= 8; $i++)
+
+	<div class="col-md-4 repository">
+
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">VDJServer</h3>
+				<h4>United States</h4>
+				<img class="logo" src="/images/repositories/vdjserver.png" />
+			</div>
+			<div class="panel-body">
+				<div class="row">
+					<div class="col-md-7">
+						<ul>
+							<li>33 studies</li>
+							<li>2949 repertoires</li>
+							<li class="nb_sequences">1.4 billion sequences</li> 
+						</ul>
+					</div>
+					<div class="col-md-5 browse-data">
+						<a role="button" class="btn btn-primary"  href="">
+							Browse data →
+						</a>
+					</div>				
+				</div>
+			</div>
+<!-- 			<div class="panel-body">
+				<h5>AIRR API</h5>
+				<ul>
+					<li>URL: https://vdjserver.org/airr/v1/info</li>
+					<li>Version: 1.0</li>
+				</ul>
+			</div> -->
+
+			<div class="panel-footer">
+				<ul>
+					<li>Website: <a class="external" target="_blank" href="https://vdjserver.org/airr/v1/info">https://vdjserver.org/airr/v1/info</a></li>
+					<li>Contact: <a class="email" href="mailto:vdjserver@utsouthwestern.edu">vdjserver@utsouthwestern.edu</a></li>
+					</ul>
+			</div>
+		</div>
+<!-- 	 <p>
+		 	Online platform for analyzing and sharing immune repertoire sequence data. 
+		 </p>
  -->
+	</div>
+	@endfor
+
+</div>
+
 
 	<table class="table table-bordered table-striped rs_list">
 				<thead>
