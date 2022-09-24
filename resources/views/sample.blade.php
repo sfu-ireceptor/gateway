b8e0eb8b64e3deefe89bdb21afa51060e5dbfd7e
jeromejaglale <jerome.jaglale@gmail.com>
2022-09-24 00:17:33 +0000

    Fix previous commit

--- resources/views/sample.blade.php
+++ resources/views/sample.blade.php
@@ -1087,4 +1087,19 @@
 							</div>
 							<div class="modal-footer">
 								<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-							</div
+							</div>
+						</div>
+					</div>
+				</div>
+				
+				@endif
+
+			</div>
+		</div>
+	</div>
+</div>
+
+@include('reloadingMessage')
+@include('loadingMessage')
+
+@stop
