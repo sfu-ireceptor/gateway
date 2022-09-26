a74bf02827e1b04641b6714ea70856f2b6c92fcf
jeromejaglale <jerome.jaglale@gmail.com>
2022-09-26 22:17:38 +0000

    Fix repertoire download not working (IR-2749)

--- app/FieldName.php
+++ app/FieldName.php
@@ -150,6 +150,7 @@ class FieldName extends Model
         $l['DataProcessing'] = 'Data Processing';
         $l['SampleProcessing'] = 'Sample Processing';
         $l['RawSequenceData'] = 'Raw Sequence Data';
+        $l['SequencingData'] = 'Sequencing Data';
         $l['PCRTarget'] = 'PCR Target';
 
         $l['Genotype'] = 'Receptor Genotype';
