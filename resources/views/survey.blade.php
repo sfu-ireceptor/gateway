65a1833af9992f1590658917bfa8fe1e9fab049a
Jerome Jaglale <jerome.jaglale@gmail.com>
2023-04-04 23:41:30 +0000

    Update survey text

--- resources/views/survey.blade.php
+++ resources/views/survey.blade.php
@@ -9,22 +9,20 @@
 	<div class="col-md-3">
 	</div>
 	<div class="col-md-6 survey_message">
+
 		<p>Dear valued iReceptor users,</p>
 
-		<p>We need your help! As you know, iReceptor is a crucial data discovery platform that facilitates the curation, analysis and sharing of AIRR-seq data and related subject and sample metadata from multiple studies across many labs and institutions. Through our platform, we have enabled researchers to collaborate and accelerate the development of vaccines, therapeutic antibodies against autoimmune diseases, and cancer immunotherapies.</p>
+		<p>We are writing to you today to ask for your support in shaping the future of iReceptor. As you know, our platform is dedicated to facilitating data discovery and sharing of Adaptive Immune Receptor Repertoire sequencing (AIRR-seq) data in the AIRR Data Commons. By bringing together antibody/B-cell and T-cell receptor repertoires from multiple studies, labs, and institutions, we are facilitating the advancement of the development of vaccines, therapeutic antibodies against autoimmune diseases, and cancer immunotherapies.</p>
 
-		<p>However, we are currently facing a challenge with funding, which is why we need your input. We have created a survey to help us determine what services you would be willing to pay for to keep iReceptor running and to continue facilitating your research.</p>
+		<p>As a grant funded research group, sustaining the iReceptor Platform operationally is very challenging, in particular when use of the platform is very high as it is currently.  We have created a survey to gather feedback from our users on which features you value the most and on which mechanisms might be practical to help with the sustainability of the platform. Your input will be instrumental in shaping the future of iReceptor and ensuring that we can continue to provide valuable resources to the research community.</p>
 
-		<p>By taking a few moments to complete this survey, you will have the opportunity to shape the future of iReceptor and help us secure the funding we need to continue providing the high-quality services you have come to rely on. Your feedback will enable us to develop sustainable business models and secure additional funding from potential investors and partners.
+		<p>We understand that your time is valuable, but we urge you to take a few minutes to complete this survey. Your feedback will help us to improve our platform, increase the value of data, and accelerate scientific discoveries.</p>
+
+		<p>Thank you for your continued support of iReceptor, and we look forward to hearing from you.</p>
+		<p>
+			Sincerely,<br>
+			The iReceptor team
 		</p>
-
-		<p>We urge you to take the survey and share it with your colleagues and peers in the research community. Your input is invaluable, and we thank you in advance for your participation.
-		</p>
-
-		<p>Together, we can ensure that iReceptor continues to facilitate groundbreaking research and enable researchers to improve human health worldwide.</p>
-
-		<p>Thank you for your support,<br>
-		The iReceptor team</p>
 
 		<div class="alert announcement" role="alert">
 			<p>
