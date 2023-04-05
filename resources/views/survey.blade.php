a250850ba6f8d04a8d268419b51137a60b643aec
Jerome Jaglale <jerome.jaglale@gmail.com>
2023-04-05 21:11:41 +0000

    Add text changes

--- resources/views/survey.blade.php
+++ resources/views/survey.blade.php
@@ -12,22 +12,21 @@
 
 		<p>Dear valued iReceptor users,</p>
 
-		<p>We are writing to you today to ask for your support in shaping the future of iReceptor. As you know, our platform is dedicated to facilitating data discovery and sharing of Adaptive Immune Receptor Repertoire sequencing (AIRR-seq) data in the AIRR Data Commons. By bringing together antibody/B-cell and T-cell receptor repertoires from multiple studies, labs, and institutions, we are facilitating the advancement of the development of vaccines, therapeutic antibodies against autoimmune diseases, and cancer immunotherapies.</p>
+		<p>We want your help shaping the future of iReceptor. As you know, our platform is dedicated to facilitating the discovery, sharing, and analysis of Adaptive Immune Receptor Repertoire sequencing (AIRR-seq) data in the AIRR Data Commons. By bringing together antibody/B-cell and T-cell receptor repertoires from multiple studies, labs, and institutions, we are facilitating the advancement of the development of vaccines, therapeutic antibodies against autoimmune diseases, and cancer immunotherapies.</p>
 
-		<p>As a grant funded research group, sustaining the iReceptor Platform operationally is very challenging, in particular when use of the platform is very high as it is currently.  We have created a survey to gather feedback from our users on which features you value the most and on which mechanisms might be practical to help with the sustainability of the platform. Your input will be instrumental in shaping the future of iReceptor and ensuring that we can continue to provide valuable resources to the research community.</p>
+		<p>As a grant funded research group, sustaining the iReceptor Platform operationally is very challenging, in particular with the growing use that we continue to see. We have created a survey to gather feedback on which features you value the most and which mechanisms might be practical to help with the sustainability of the platform.We understand that your time is valuable, but we urge you to take the time to complete this survey. Your input will be instrumental in shaping the future of iReceptor and ensuring that we can continue to provide this valuable resource to the research community.</p>
 
-		<p>We understand that your time is valuable, but we urge you to take a few minutes to complete this survey. Your feedback will help us to improve our platform, increase the value of data, and accelerate scientific discoveries.</p>
+		<p>Thank you for your continued support of iReceptor. We look forward to hearing from you.</p>
 
-		<p>Thank you for your continued support of iReceptor, and we look forward to hearing from you.</p>
 		<p>
 			Sincerely,<br>
 			The iReceptor team
 		</p>
 
-		<div class="alert announcement" role="alert">
+		<div class="announcement" role="alert">
 			<p>
 				<a  class="btn btn-success external"  target="_blank" role="button" href="/ireceptor-survey-go">Take the iReceptor Survey</a><br>
-				<a href="">Skip and go to home page</a>
+				<a href="/home">Skip for now</a>
 			</p>
 		</div>
 
