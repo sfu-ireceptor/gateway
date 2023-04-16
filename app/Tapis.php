916e17ec3fc5ce0915fb82c0be01604e1ace798b
bcorrie <bdcorrie@gmail.com>
2023-04-16 23:15:56 +0000

    Extra debug info on Client errors.

--- app/Tapis.php
+++ app/Tapis.php
@@ -1014,7 +1014,7 @@ class Tapis
             Log::error('Tapis::doHTTPRequest:: ClientException - query = ' . $url);
             Log::error('Tapis::doHTTPRequest:: ClientException - response = ' . $response);
             $this->raiseExceptionIfTapisError($response);
-
+            Log::error('Tapis::doHTTPRequest:: ClientException - returning response = ' . $response);
             return $response;
         } catch (RequestException $exception) {
             $response = $exception->getResponse()->getBody()->getContents();
