ca33d27694be321432d096d6097bdf0e561c1343
Brian Corrie <bdcorrie@gmail.com>
2023-11-02 23:32:36 +0000

    Ignore further notifications when a job is STOPPED.

--- app/Jobs/ProcessJobNotification.php
+++ app/Jobs/ProcessJobNotification.php
@@ -46,8 +46,9 @@ class ProcessJobNotification implements ShouldQueue
             return;
         }
 
-        // ignore the status update if the job has already FAILED or is FINISHED
-        if ($job->agave_status == 'FAILED' || $job->agave_status == 'FINISHED') {
+        // ignore the status update if the job has already FAILED, FINISHED, or STOPPED
+	if ($job->agave_status == 'FAILED' || $job->agave_status == 'FINISHED' ||
+	    $job->agave_status == 'STOPPED' ) {
             $localJob->setFinished();
 
             return;
