e95079c17c6f5a23244e72c2206b178a8e68214f
Brian Corrie <bdcorrie@gmail.com>
2024-03-20 21:38:25 +0000

    Experimental blades for jobs using new download refactor

---
+++ resources/views/job/css_file.blade.php
@@ -0,0 +1,18 @@
+@extends('template')
+
+@section('title', $job->app . ' (Job ' . $job->id . ') ' . $job->agave_status)
+@section('base_title', ' ')
+@section('base', asset('jobs/view/file/'.$job->id.'/'))
+
+@section('content')
+<div class="container job_container">
+{{ asset('jobs/view/file/'.$job->id.'?file=')}}
+    @if (count($plain_file) > 0)
+        @foreach ($plain_file as $line)
+	    {!! $line !!}
+        @endforeach
+    @else
+	File is empty
+    @endif
+</div>
+@stop
