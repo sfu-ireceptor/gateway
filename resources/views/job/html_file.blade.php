293fe4d1874f882869c23d4c1f4d0e930cfc760c
Brian Corrie <bdcorrie@gmail.com>
2024-03-25 20:15:18 +0000

    Additions to job file viewing blade/controller.

--- resources/views/job/html_file.blade.php
+++ resources/views/job/html_file.blade.php
@@ -2,17 +2,15 @@
 
 @section('title', $job->app . ' (Job ' . $job->id . ') ' . $job->agave_status)
 @section('base_title', ' ')
-<!--
-@section('base', asset('jobs/view/file/'.$job->id.'/'))
--->
-@section('base', asset('/storage/Total/'))
 
 @section('content')
 <div class="container job_container">
-{{ asset('jobs/view/file/'.$job->id.'?file=')}}
     @if (count($plain_file) > 0)
         @foreach ($plain_file as $line)
-	    {!! $line !!}
+        <!--
+            {!! preg_replace($pattern, $replace_str, $line) !!}
+        -->
+            {!! $line !!}
         @endforeach
     @else
 	File is empty
