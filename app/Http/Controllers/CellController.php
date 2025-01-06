a6c773855944c15bcb85bb1ed1a9a9d142cb1ac0
Brian Corrie <bdcorrie@gmail.com>
2025-01-06 21:58:07 +0000

    Refactor of filter and service processing

--- app/Http/Controllers/CellController.php
+++ app/Http/Controllers/CellController.php
@@ -69,83 +69,11 @@ class CellController extends Controller
 
         // Convert the service repertoire lists into an associative array with key
         // the ID and the contents an array of repertoire_ids.
-        $service_repertoire_list = [];
-        foreach ($filters as $key => $value) {
-            if (strrpos($key, 'ir_project_sample_id_list') !== false) {
-                // Use everything after the last "_" as the ID
-                $id_str = substr($key, strrpos($key, '_') + 1);
-                $service_repertoire_list[$id_str] = $value;
-            }
-        }
+        $service_repertoire_list = $this->getServiceRepertoires($filters);
         //var_dump($service_repertoire_list);
 
-        // Extract the cell, expression, and reactivity specific filters.
-        $cell_filters = [];
-        $expression_filters = [];
-        $reactivity_filters = [];
-        //var_dump($basic_filters);
-        foreach ($filters as $key => $value) {
-            // Each key has the filter type encoded in the name after the last "_", as in
-            // virtual_pairing_cell is a virtual_pairing filter of type cell
-            $sep_location = strrpos($key, '_');
-            // For each type of filter, add it to the filter list
-            if ($sep_location !== false) {
-                $filter_type = substr($key, $sep_location + 1);
-                if ($filter_type == 'cell' && $value != null) {
-                    $cell_filters[$key] = $value;
-                } elseif ($filter_type == 'expression' && $value != null) {
-                    $expression_filters[$key] = $value;
-                } elseif ($filter_type == 'reactivity' && $value != null) {
-                    $reactivity_filters[$key] = $value;
-                }
-            }
-            // Each query type requires the repertoires, which are encoded with keys
-            // that contain the string "ir_project_sample_id_list"
-            /*
-            if (strrpos($key, 'ir_project_sample_id_list') !== false) {
-                $cell_filters[$key] = $value;
-                $expression_filters[$key] = $value;
-                $reactivity_filters[$key] = $value;
-            }
-            */
-        }
-        //var_dump($cell_filters);
-        //var_dump($expression_filters);
-        //var_dump($reactivity_filters);
-
-        // allow only Cell filters, or only GEX filters, or only Reactivity filters,
-        // based on currently opened panel. Panel 0 is Cell, Panel 1 Reactivity,
-        // Panel 2 Expression
-        /*
-        if (isset($filters['open_filter_panel_list'])) {
-            $open_filter_panel_list = $filters['open_filter_panel_list'];
-            if ($open_filter_panel_list[0] == '0') {
-                // No reactivity, no expression, we just want cell filters
-                unset($filters['property_expression']);
-                unset($filters['value_expression']);
-                unset($filters['antigen_reactivity']);
-                unset($filters['antigen_source_species_reactivity']);
-                unset($filters['peptide_sequence_aa_reactivity']);
-            } elseif ($open_filter_panel_list[0] == '1') {
-                // No cell, no expression, we just want reactivity filters
-                unset($filters['cell_id_cell']);
-                unset($filters['expression_study_method_cell']);
-                unset($filters['virtual_pairing_cell']);
-                unset($filters['property_expression']);
-                unset($filters['value_expression']);
-            } elseif ($open_filter_panel_list[0] == '2') {
-                // No cell, no reactivity, we just want expression filters
-                unset($filters['cell_id_cell']);
-                unset($filters['expression_study_method_cell']);
-                unset($filters['virtual_pairing_cell']);
-                unset($filters['antigen_reactivity']);
-                unset($filters['antigen_source_species_reactivity']);
-                unset($filters['peptide_sequence_aa_reactivity']);
-            }
-        }
-         */
-        //var_dump($filters);
-        //var_dump($service_repertoire_list);
+        $object_filters = $this->getCellObjectFilters($filters);
+        //var_dump($object_filters);
         $new_filters = $filters;
         unset($new_filters['cell_id_cell']);
         unset($new_filters['expression_study_method_cell']);
@@ -156,9 +84,12 @@ class CellController extends Controller
         unset($new_filters['antigen_source_species_reactivity']);
         unset($new_filters['peptide_sequence_aa_reactivity']);
         // Retrieve cell data given the filters.
+        //$cell_data = Cell::summary($service_repertoire_list, $new_filters,
+        //    $cell_filters, $expression_filters,
+        //    $reactivity_filters, $username);
         $cell_data = Cell::summary($service_repertoire_list, $new_filters,
-            $cell_filters, $expression_filters,
-            $reactivity_filters, $username);
+            $object_filters['cell'], $object_filters['expression'],
+            $object_filters['reactivity'], $username);
         //var_dump($cell_data['items']);
         //Log::debug($cell_data);
         //blah;
@@ -491,6 +422,52 @@ class CellController extends Controller
         // display view
         return view('cell', $data);
     }
+
+    public function getServiceRepertoires($filters)
+    {
+        // Convert the service repertoire lists into an associative array with key
+        // the ID and the contents an array of repertoire_ids.
+        $service_repertoire_list = [];
+        foreach ($filters as $key => $value) {
+            if (strrpos($key, 'ir_project_sample_id_list') !== false) {
+                // Use everything after the last "_" as the ID
+                $id_str = substr($key, strrpos($key, '_') + 1);
+                $service_repertoire_list[$id_str] = $value;
+            }
+        }
+        return $service_repertoire_list;
+    }
+
+    public function getCellObjectFilters($filters)
+    {
+        // Extract the cell, expression, and reactivity specific filters.
+        $cell_filters = [];
+        $expression_filters = [];
+        $reactivity_filters = [];
+        //var_dump($basic_filters);
+        foreach ($filters as $key => $value) {
+            // Each key has the filter type encoded in the name after the last "_", as in
+            // virtual_pairing_cell is a virtual_pairing filter of type cell
+            $sep_location = strrpos($key, '_');
+            // For each type of filter, add it to the filter list
+            if ($sep_location !== false) {
+                $filter_type = substr($key, $sep_location + 1);
+                if ($filter_type == 'cell' && $value != null) {
+                    $cell_filters[$key] = $value;
+                } elseif ($filter_type == 'expression' && $value != null) {
+                    $expression_filters[$key] = $value;
+                } elseif ($filter_type == 'reactivity' && $value != null) {
+                    $reactivity_filters[$key] = $value;
+                }
+            }
+        }
+        $object_filters = [];
+        $object_filters['reactivity'] = $reactivity_filters;
+        $object_filters['expression'] = $expression_filters;
+        $object_filters['cell'] = $cell_filters;
+        return $object_filters;
+    }
+
 
     public function timeEstimate($nb_cells)
     {
