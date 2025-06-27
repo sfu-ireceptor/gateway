00c152f9a41ba619c326a5c198383923e2d37578
Brian Corrie <bdcorrie@gmail.com>
2025-06-27 21:03:44 +0000

    Add method to look up name given an ID

--- app/Antigens.php
+++ app/Antigens.php
@@ -3,10 +3,39 @@
 namespace App;
 
 use Jenssegers\Mongodb\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
+
 
 class Antigens extends Model
 {
     protected $connection = 'mongodb'; // https://github.com/jenssegers/laravel-mongodb
     protected $collection = 'antigens';
     protected $guarded = [];
+
+    public static function lookup_antigen_name($antigen_id)
+    {
+        $antigen_name = '';
+        try {
+            $defaults = [];
+            $defaults['verify'] = false;    // accept self-signed SSL certificates
+
+            $antigen_array = explode(':', $antigen_id);
+            if (count($antigen_array) == 2) {
+                $defaults['base_uri'] = 'https://rest.uniprot.org/uniprotkb/';
+                $client = new \GuzzleHttp\Client($defaults);
+                $response = $client->get($antigen_array[1].'.json');
+                $body = $response->getBody();
+                $t = json_decode($body);
+                // TODO: Check that these keys exist for object $t
+                $antigen_name = $t->proteinDescription->recommendedName->fullName->value;
+            }
+        } catch (\Exception $e) {
+            $error_message = $e->getMessage();
+            Log::error('Antigens: UNIProt request failed: ' . $error_message);
+            $antigen_name = '';
+        }
+
+        return $antigen_name;
+    }
+
 }
