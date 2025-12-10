8a08c8b274dd678c46e118a71ab28c70e1342dc7
Brian Corrie <bdcorrie@gmail.com>
2025-12-10 22:37:11 +0000

    Change messages to reflect not logged in after password reset.

--- resources/views/user/resetPasswordConfirmation.blade.php
+++ resources/views/user/resetPasswordConfirmation.blade.php
@@ -10,8 +10,7 @@
 	<div class="row">
 
 		<div class="col-md-6">
-			<p>We emailed you the new password. But no need to enter it right now, we logged you in :)</p>
-			<p><a href="/home">Go to home page.</a></p>
+			<p>Please go to the <a href="/login">login page</a> and login with the credenitals sent to you in your email.</p>
 		</div>
 
 	</div>
