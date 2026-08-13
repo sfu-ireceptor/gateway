61b673c6f6fd737962d5b1d6c512eaa5c921bdfd
Brian Corrie <bdcorrie@gmail.com>
2026-08-13 20:03:11 +0000

    Deployment test

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
 <div class="banner_title samples">
-    <h1>The iReceptor Gateway is down for maintenance </h1>
+    <h1>The iReceptor Gateway is down for maintenance</h1>
     <p> {!! config('ireceptor.message_503_error') !!}
     </p>
 @endsection
