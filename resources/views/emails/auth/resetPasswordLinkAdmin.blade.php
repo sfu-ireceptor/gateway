173762bc40a65a8753afda79e570c4431851d06b
Brian Corrie <bdcorrie@gmail.com>
2026-08-16 15:36:53 +0000

    Improve email message on what will happen

--- resources/views/emails/auth/resetPasswordLinkAdmin.blade.php
+++ resources/views/emails/auth/resetPasswordLinkAdmin.blade.php
@@ -1,12 +1,16 @@
 Hi {{ $first_name }},
 
 This email is registered as the contact email for an account
-on the iReceptor Gateway (https://gatewya.ireceptor.org). 
+on the iReceptor Gateway (https://gateway.ireceptor.org). 
 The iReceptor Admin team has requested that you confirm the 
 validity of this email and update your password. 
 
 To reset your iReceptor password, please click on this link:
 {{ $reset_link }}
+Clicking on this link will mark your email as verified and will
+then generate a second email with a new password. Please log in
+to the iReceptor Gateway with this password, go to your user
+account page, and reset your password.
 
 If you have questions/concerns about this request please email 
 support@ireceptor.org to confirm the validity of this request.
