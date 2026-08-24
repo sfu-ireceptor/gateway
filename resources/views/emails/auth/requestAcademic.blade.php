4f94721a6facd4a512391545ba4a262a8f2c63d9
Brian Corrie <bdcorrie@gmail.com>
2026-08-24 16:29:03 +0000

    Add ability for user to ask for Academic subscription

---
+++ resources/views/emails/auth/requestAcademic.blade.php
@@ -0,0 +1,8 @@
+iReceptor Support,
+
+The user {{$username}} has requested a subscription upgrade to
+an Academic user. Please review the user's email ({{$email}}} 
+to determine if they are qualified and approve on the iReceptor
+Gateway users page.
+
+
