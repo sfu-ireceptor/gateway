c09066e16e841c2314516dff8aa6adc61a3225df
Brian Corrie <bdcorrie@gmail.com>
2026-08-25 23:11:52 +0000

    Wording changes to messaging to users

--- resources/views/confirmInfo.blade.php
+++ resources/views/confirmInfo.blade.php
@@ -10,33 +10,33 @@
 <p>
 As of August 31st, 2026, the iReceptor Gateway is using subscription 
 levels to control access to the services provided by the gateway. See our
-<a ref="/subscriptions">Subscriptions page</a> for more details.
+<a ref="/subscriptions">Subscriptions page</a> for more details on why
+subscriptions are necessary.
 
 There are currently two levels of subscription, a free Academic subscription and
 a paid Commercial subscription. If the email you use for your account is from
 a recognized academic institution, you will automatically be granted
-an Academic subscription.
-If you have paid for a Commercial subscription, you will have a Commercial
-subscription. If neither of these apply to you, you will temporarily be
-assigned a Limited subscription, allowing you to either apply for an Academic
+an <b>Academic</b> subscription.
+If the email associated with your iReceptor Gateway account is incorrect, you can go to your <a href=/user/account>user account page</a> to change the email. 
+If you have a paid subscription your subscription status will be <b>Commercial</b>.
+If neither of these apply to you, you will temporarily be
+assigned a <b>Limited</b> subscription, allowing you to either apply for an Academic
 subscription (by changing your account email to use your academic email)
 or pay for a Commercial subscription using the <a href="/register">Registration page</a>.
 <p>
 Your current subscription level is: <b>{{ $status }}</b>
+<p>
+Your account has temporarily been assigned reduced access on the
+iReceptor Gateway until your email has been confirmed.
+Once you have clicked on the email confirmation link,
+you will be given
+access according to the status of your iReceptor Gateway subscription.
 <p>
 We have also changed our <a href="/terms">Terms and Conditions</a> and 
 <a href="/privacy-policy">Privacy Policy</a>. Please review these changes.
 <p>
 To confirm your email address and acknowledge that you agree to adhere 
 to these terms and policies, please click on the button below. 
-Your account has temporarily been assigned reduced access on the
-iReceptor Gateway. Once you have clicked on the email confirmation link,
-you will be given
-access according to the status of your iReceptor Gateway subscription.
-</p>
-
-<p>
-If the email associated with your iReceptor Gateway account is incorrect, you can go to your <a href=/user/account>user account page</a> to change the email. 
 </p>
 
 		<p>
@@ -46,7 +46,7 @@ If the email associated with your iReceptor Gateway account is incorrect, you ca
 
 		<div class="announcement" role="alert">
 			<p>
-				<a  class="btn btn-success"  role="button" href="/confirm-info-ok">I agree to these Terms and Policies</a><br>
+				<a  class="btn btn-success"  role="button" href="/confirm-info-ok">Confirm Email and agree to our Terms and Policies</a><br>
 			</p>
 		</div>
 	</div>
--- resources/views/user/account.blade.php
+++ resources/views/user/account.blade.php
@@ -28,9 +28,18 @@
 					<p>
                         <strong>Subscription</strong>
                         <br /> {{ $user->status}}
+	                    @if (str_contains($user->status, "Commercial"))
+                            (<a href="https://billing.stripe.com/p/login/bJecN7bpo0oObx6alBbfO00">Manage your subscription</a>)
+                        @endif
 	                    @if (str_contains($user->status, "Limited"))
-                            <p>Note: iReceptor uses subscriptions to manage access to the iReceptor Gateway. You have a "Limited" account because you have either not confirmed your Academic email or have not paid for a Commercial subscription. If you are an academic user, please change the email you are using to your academic email in your personal information. If you are an academic user and for some reason your academic email is not currently approved (or you can not use your academic email), you can request an Academic Upgrade below. If you choose this option, the iReceptor team will contact you to determine your eligibility for an Academic subscription. If you are a Commercial user, please go to the <a href="/register">Subscribe</a> page and choose a commercial subscription package.
-                            <p><a href="/user/request-academic-upgrade">Request Academic Upgrade</a>
+<p>Note: iReceptor uses subscriptions to manage access to the iReceptor Gateway. You have a "Limited" account because you have either not confirmed your Academic email or have not paid for a Commercial subscription.
+
+<p> If you are an academic user, please change the email you are using to your academic email in your personal information.
+
+<p> If you are using an academic email and still have a limited account, this is because your academic email is not from an easily identified academic domain name. Please can request an Academic Approval below. If you choose this option, the iReceptor team will contact you to determine your eligibility for an Academic subscription.
+<br><a href="/user/request-academic-upgrade">Request Academic Approval</a>
+
+<p> If you are a Commercial user, please go to the <a href="/register">Subscribe</a> page and choose a commercial subscription package.
                         @endif
                     </p>
 			  </div>
